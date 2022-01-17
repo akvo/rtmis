17187259dff3c6b7d9655c51f0cefe4708a2f717
dedenbangkit <mail@dedenbangkit.com>
2022-01-17 10:22:55 +0700

    [#1] DB setup and pgadmin

---
+++ db/docker-entrypoint-initdb.d/000-init.sql
@@ -0,0 +1,8 @@
+CREATE USER akvo WITH CREATEDB PASSWORD 'password';
+
+CREATE DATABASE rtmis
+WITH OWNER = akvo
+     TEMPLATE = template0
+     ENCODING = 'UTF8'
+     LC_COLLATE = 'en_US.UTF-8'
+     LC_CTYPE = 'en_US.UTF-8';
