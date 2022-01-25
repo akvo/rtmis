5851c9ab4da0b2bc938bbc40c6bdf6be3b34d21e
dedenbangkit <mail@dedenbangkit.com>
2022-01-25 09:29:59 +0700

    [#12] Update dbdocs script

--- doc/update.sh
+++ doc/update.sh
@@ -1,1 +1,9 @@
-dbdocs build schema.dbml --project rtmis
+#!/usr/bin/env bash
+
+if ! dbdocs -v dbdocs > /dev/null
+then
+    echo "Please install dbdocs CLI! https://dbdocs.io/docs"
+    exit
+else
+    dbdocs build schema.dbml --project rtmis
+fi
