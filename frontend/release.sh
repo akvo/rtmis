7a7bb01a8f014b7351a3b8efcbe61fc07491e65e
dedenbangkit <mail@dedenbangkit.com>
2022-01-26 22:55:57 +0700

    [#11] Sed replace eslint config for CI

--- frontend/release.sh
+++ frontend/release.sh
@@ -3,7 +3,9 @@
 
 set -euo pipefail
 
+sed -i 's/"warn"/"error"/g' < .eslintrc.json > .eslintrc.prod.json
+
 yarn install --no-progress --frozen-lock
-yarn eslint src/
+yarn eslint -c .eslintrc.prod.json src/
 yarn prettier --check src/
 yarn build
