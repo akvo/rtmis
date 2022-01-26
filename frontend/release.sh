3045bbd82ee77ee5529cc95d98c56bd52a972691
dedenbangkit <mail@dedenbangkit.com>
2022-01-26 23:11:22 +0700

    [#11] Move eslint replicas to build script

--- frontend/release.sh
+++ frontend/release.sh
@@ -3,8 +3,6 @@
 
 set -euo pipefail
 
-sed -i 's/"warn"/"error"/g' < .eslintrc.json > .eslintrc.prod.json
-
 yarn install --no-progress --frozen-lock
 yarn eslint -c .eslintrc.prod.json src/
 yarn prettier --check src/
