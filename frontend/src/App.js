68c3eb715a9f7aac96073614cb769d95e856d316
dedenbangkit <mail@dedenbangkit.com>
2022-01-26 13:51:39 +0700

    [#9] Router dom and Navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,26 @@
 import "./App.scss";
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import { Layout } from "./components";
+import { Home } from "./pages";
+
+const history = createBrowserHistory();
 
 const App = () => {
   return (
-    <Layout>
-      <Layout.Header>Test</Layout.Header>
-      <Layout.Body>
-        <div>test</div>
-      </Layout.Body>
-      <Layout.Footer>Test</Layout.Footer>
-    </Layout>
+    <Router history={history}>
+      <Layout>
+        <Layout.Header title="Ministry of Health" />
+        <Layout.Body>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/data" element={<Home />} />
+          </Routes>
+        </Layout.Body>
+        <Layout.Footer>Test</Layout.Footer>
+      </Layout>
+    </Router>
   );
 };
 
--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,20 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Row, Col, Space } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
-const Header = ({ children, className = "header", ...props }) => {
+const Header = ({ title = "Site Title", className = "header", ...props }) => {
   return (
-    <div className={className} {...props}>
-      {children}
-    </div>
+    <Row
+      className={className}
+      align="middle"
+      justify="space-between"
+      {...props}
+    >
+      <Col>
+        <img className="logo" src="/logo.png" />
+        <h1 className="title">{title}</h1>
+      </Col>
+      <Col className="navigation">
+        <Space>
+          <Link to="/"> Data</Link>
+          <Link to="/"> Reports</Link>
+          <Link to="/"> Monitoring</Link>
+          <Link to="/"> How We Work</Link>
+        </Space>
+      </Col>
+      <Col className="menu">
+        <a href={"/menu"}>
+          John Doe
+          <span className="icon">
+            <UserOutlined />
+          </span>
+        </a>
+      </Col>
+    </Row>
   );
 };
 
 Header.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.element,
-    PropTypes.node,
-  ]).isRequired,
+  title: PropTypes.string,
   className: PropTypes.string,
 };
 
