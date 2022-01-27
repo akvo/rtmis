d99b5ff5825496eb6fb6b221925de60871a48d9c
dedenbangkit <mail@dedenbangkit.com>
2022-01-27 22:41:24 +0700

    [#20] Login Page Content and Form

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.scss";
 import { Row, Col } from "antd";
+import LoginForm from "./LoginForm";
 import backgroundImage from "../../assets/banner.png";
 import { config } from "../../lib";
 
@@ -39,6 +40,7 @@ const Login = () => {
             <br />
             <small>Please enter your account details</small>
           </h1>
+          <LoginForm />
         </Col>
       </Row>
     </div>
---
+++ frontend/src/pages/login/LoginForm.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+
+const LoginForm = () => {
+  const onFinish = (values) => {
+    console.info("Received values of form: ", values);
+  };
+
+  return (
+    <Form
+      name="login-form"
+      layout="vertical"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="email"
+        label="Email Address"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Username!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Email"
+        />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Password!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <a className="login-form-forgot" href="">
+          Forgot password
+        </a>
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default LoginForm;
