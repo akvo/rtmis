1a18c7756dfc7d03a31b6eabc846e4f46cd43296
zuhdil <zuhdil@gmail.com>
2022-01-31 11:33:59 +0700

    [#13] Run tests and coverage on CI

--- ci/build.sh
+++ ci/build.sh
@@ -52,10 +52,9 @@ backend_build () {
         --tag "${image_prefix}/backend:${CI_COMMIT}" backend
 
     # Code Quality
-    dc -f docker-compose.test.yml run \
+    dc -f docker-compose.test.yml -p backend-test run \
         --rm \
-        --no-deps \
-        backend flake8
+        backend ./run-qc.sh
 
     # Update dbdocs.io
     if [[ "${CI_BRANCH}" ==  "main" || "${CI_BRANCH}" ==  "develop" ]]; then
