454647c236dcc3910bd3569513ad3eff690c51e3
zuhdil <zuhdil@gmail.com>
2022-01-31 11:26:50 +0700

    [#13] Setup code coverage

--- backend/build.sh
+++ backend/build.sh
@@ -6,6 +6,12 @@ echo "Running lint"
 flake8
 
 echo "Running tests"
-./manage.py test --shuffle --parallel 4
+COVERAGE_PROCESS_START=./.coveragerc \
+  coverage run --parallel-mode --concurrency=multiprocessing --rcfile=./.coveragerc \
+  manage.py test --shuffle --parallel 4
+
+echo "Coverage"
+coverage combine --rcfile=./.coveragerc
+coverage report -m --rcfile=./.coveragerc
 
 echo "Done"
