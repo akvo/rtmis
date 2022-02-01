564e370c47bbbd81103d0d64a96a75e9bfc4583a
dedenbangkit <mail@dedenbangkit.com>
2022-02-01 21:11:07 +0700

    [#25] Form seeder commmand pt-2

--- backend/api/v1/v1_forms/management/commands/form_seeder.py
+++ backend/api/v1/v1_forms/management/commands/form_seeder.py
@@ -1,7 +1,8 @@
 import os
 import json
 from django.core.management import BaseCommand
-from api.v1.v1_forms.models import Forms, Questions, QuestionGroup
+from api.v1.v1_forms.models import Forms, QuestionGroup
+from api.v1.v1_forms.models import Questions, QuestionOptions
 from api.v1.v1_forms.constants import QuestionTypes, FormTypes
 
 source_folder = './source/forms/'
@@ -32,20 +33,30 @@ class Command(BaseCommand):
                     })
                 print(f"- {question_group.name}")
                 for iq, q in enumerate(qg["questions"]):
-                    id = q["id"] if "id" in q else int(form.id) + iq
                     question, _ = Questions.objects.update_or_create(
-                        id=id,
+                        id=q["id"] if "id" in q else None,
                         name=q["question"],
                         text=q["question"],
                         form=form,
                         question_group=question_group,
-                        rule=q["rule"] if "rule" in q else None,
+                        rule=getattr(q, "rule", None),
                         required=q["required"] if "required" in q else None,
                         dependency=q["dependency"]
                         if "dependency" in q else None,
                         type=getattr(QuestionTypes, q["type"]),
                         defaults={
-                            'id': id,
+                            'name': q["question"],
                             'form': form,
                             'question_group': question_group
                         })
+                    if getattr(q, "options", None):
+                        for o in q["options"]:
+                            print(o)
+                            QuestionOptions.objects.update_or_create(
+                                question=question,
+                                name=o["name"],
+                                defaults={
+                                    'name': o["name"],
+                                    'code': o["code"] if "code" in o else None,
+                                    'question': question
+                                })
