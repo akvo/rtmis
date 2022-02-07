3e4691497966bab4edef525ef297efb34a9e51a8
dedenbangkit <mail@dedenbangkit.com>
2022-02-08 05:44:15 +0700

    [#36] Add watchAll flag

--- frontend/release.sh
+++ frontend/release.sh
@@ -6,5 +6,5 @@ set -euo pipefail
 yarn install --no-progress --frozen-lock
 yarn eslint --config .eslintrc.prod.json src --ext .js,.jsx
 yarn prettier --check src/
-yarn test
+yarn test --watchAll=false
 yarn build
