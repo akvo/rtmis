58a215508983fc3221adedb8b0ed9e3ed5d1914e
dedenbangkit <mail@dedenbangkit.com>
2022-02-08 04:58:54 +0700

    [#36] Add example test, include test to CI

--- frontend/release.sh
+++ frontend/release.sh
@@ -6,4 +6,5 @@ set -euo pipefail
 yarn install --no-progress --frozen-lock
 yarn eslint --config .eslintrc.prod.json src --ext .js,.jsx
 yarn prettier --check src/
+yarn test
 yarn build
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,52 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { CookiesProvider } from "react-cookie";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
+
+const TestApp = ({ entryPoint = "/" }) => {
+  return (
+    <CookiesProvider>
+      <MemoryRouter initialEntries={[entryPoint]}>
+        <App />
+      </MemoryRouter>
+    </CookiesProvider>
+  );
+};
+
+describe("App", () => {
+  test("test if the login button exists", () => {
+    render(<TestApp />);
+    const linkElement = screen.getByText(/Log In/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("test if the login form exists", () => {
+    render(<TestApp />);
+    userEvent.click(screen.getByText(/Log In/i), { button: 0 });
+    expect(screen.getByText(/Welcome back/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(/Please enter your account details/i)
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Email Address/i)).toBeInTheDocument();
+    expect(screen.getByText(/Remember me/i)).toBeInTheDocument();
+    expect(screen.getByText(/Forgot password/i)).toBeInTheDocument();
+  });
+
+  test("test if the registration form is checking the endpoint", () => {
+    render(<TestApp entryPoint={"/login/abcd"} />);
+    expect(screen.getByText(/Verifying/i)).toBeInTheDocument();
+  });
 });
