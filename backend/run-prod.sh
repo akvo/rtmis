1f3816130d4f7d732bc5a840a241686542f28c53
dedenbangkit <mail@dedenbangkit.com>
2022-02-07 17:08:56 +0700

    [#10] Remove seeder and migration command from prod

--- backend/run-prod.sh
+++ backend/run-prod.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2155
 
-python manage.py migrate
-python manage.py administration_seeder
+# python manage.py migrate
+# python manage.py administration_seeder
 gunicorn --bind 0.0.0.0:8000 rtmis.wsgi:application
