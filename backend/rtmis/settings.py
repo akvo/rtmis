8ac3c0add991b76c3fe289eae5ad82d7189b2fde
dedenbangkit <mail@dedenbangkit.com>
2022-02-07 16:13:19 +0700

    [#10] Add allowed host, gunicorn bind to default

--- backend/rtmis/settings.py
+++ backend/rtmis/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = environ["DJANGO_SECRET"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'rtmis.akvo.org']
+ALLOWED_HOSTS = ['localhost', 'rtmis.akvo.org', 'rtmis.akvotest.org']
 
 # Application definition
 
--- backend/run-prod.sh
+++ backend/run-prod.sh
@@ -3,4 +3,4 @@
 
 python manage.py migrate
 python manage.py administration_seeder
-gunicorn --bind 0.0.0.0:8000 rtmis.wsgi:application
+gunicorn rtmis.wsgi:application
