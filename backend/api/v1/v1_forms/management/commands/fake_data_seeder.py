3ee1966bd139dd5707ca8c14899692b662ab5d48
dedenbangkit <mail@dedenbangkit.com>
2022-02-11 01:43:59 +0700

    [#42] Replace form, fix fake data seeder

--- backend/api/v1/v1_forms/management/commands/fake_data_seeder.py
+++ backend/api/v1/v1_forms/management/commands/fake_data_seeder.py
@@ -2,11 +2,12 @@ from datetime import timedelta
 
 from django.core.management import BaseCommand
 from django.utils import timezone
+from django.db.transaction import atomic
 from faker import Faker
 
 from api.v1.v1_forms.constants import QuestionTypes
 from api.v1.v1_forms.models import Forms, FormData, Answers
-from api.v1.v1_profile.models import Administration
+from api.v1.v1_profile.models import Administration, Levels
 from api.v1.v1_users.models import SystemUser
 
 fake = Faker()
@@ -24,14 +25,20 @@ def add_fake_answers(data: FormData):
             lat, lng = fake.latlng()
             option = [str(lat), str(lng)]
         elif question.type == QuestionTypes.administration:
-            value = str(Administration.objects.order_by('?').first().pk)
+            administration = Administration.objects.filter(
+                level=Levels.objects.order_by('level').last().level).order_by(
+                    '?').first()
+            name = administration.name
+            value = administration.id
         elif question.type == QuestionTypes.text:
-            name = fake.text(max_nb_chars=10)
+            name = fake.company() if question.meta else fake.text(
+                max_nb_chars=10)
         elif question.type == QuestionTypes.number:
             value = fake.random_int(min=10, max=50)
         elif question.type == QuestionTypes.option:
             option = [
-                question.question_question_options.order_by('?').first().name]
+                question.question_question_options.order_by('?').first().name
+            ]
         elif question.type == QuestionTypes.multiple_option:
             option = list(
                 question.question_question_options.order_by('?').values_list(
@@ -50,41 +57,45 @@ def add_fake_answers(data: FormData):
                 meta_name.append(name)
             elif option and question.type != QuestionTypes.geo:
                 meta_name.append(','.join(option))
-            else:
+            elif value:
                 meta_name.append(value)
+            else:
+                pass
 
+        name = name if question.type != QuestionTypes.administration else None
         Answers.objects.create(
             data=data,
             question=question,
             name=name,
             value=value,
             options=option,
-            created_by=SystemUser.objects.order_by('?').first()
-        )
+            created_by=SystemUser.objects.order_by('?').first())
     data.name = ' - '.join(meta_name)
     data.save()
 
 
 class Command(BaseCommand):
-
     def add_arguments(self, parser):
         parser.add_argument("-r",
                             "--repeat",
-                            default=1,
+                            nargs="?",
+                            const=20,
+                            default=20,
                             type=int)
 
+    @atomic
     def handle(self, *args, **options):
 
         FormData.objects.all().delete()
         lat, lng = fake.latlng()
         geo = {'lat': str(lat), 'lng': str(lng)}
-        for form in Forms.objects.all()[:1]:
+        for form in Forms.objects.all():
+            print(f"{form.id} - {form.name}")
             data = FormData.objects.create(
                 name=fake.pystr_format(),
                 form=form,
                 administration=Administration.objects.order_by('?').first(),
                 geo=geo,
-                created_by=SystemUser.objects.order_by('?').first()
-            )
+                created_by=SystemUser.objects.order_by('?').first())
             for i in range(options.get('repeat')):
                 add_fake_answers(data)
