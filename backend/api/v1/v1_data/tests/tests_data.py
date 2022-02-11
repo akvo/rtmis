65640122bc3681b3725d25aec544a36a3f27206a
Nirav <nirav.rajguru@9spl.com>
2022-02-11 14:19:15 +0530

    [#42] Form Data list with Pagination order by created or updated

---
+++ backend/api/v1/v1_data/tests/tests_data.py
@@ -0,0 +1,25 @@
+from django.core.management import call_command
+from django.test import TestCase
+
+
+class DataTestCase(TestCase):
+    def test_list_form_data(self):
+        self.maxDiff = None
+        call_command("administration_seeder", "--test")
+        user_payload = {"email": "admin@rtmis.com", "password": "Test105*"}
+        user_response = self.client.post('/api/v1/login/',
+                                         user_payload,
+                                         content_type='application/json')
+        token = user_response.json().get('token')
+
+        call_command("form_seeder", "--test")
+        header = {
+            'HTTP_AUTHORIZATION': f'Bearer {token}'
+        }
+
+        data = self.client.get("/api/v1/list/form-data/1?page=1", follow=True,
+                               **header)
+        self.assertEqual(data.status_code, 200)
+        data = self.client.get("/api/v1/list/form-data/1?page=2", follow=True,
+                               **header)
+        self.assertEqual(data.status_code, 404)
--- backend/api/v1/v1_data/urls.py
+++ backend/api/v1/v1_data/urls.py
@@ -1,7 +1,9 @@
 from django.urls import re_path
 
-from api.v1.v1_data.views import submit_form
+from api.v1.v1_data.views import submit_form, list_form_data
 
 urlpatterns = [
     re_path(r'^(?P<version>(v1))/form-data/(?P<pk>[0-9]+)/', submit_form),
+    re_path(r'^(?P<version>(v1))/list/form-data/(?P<pk>[0-9]+)/',
+            list_form_data),
 ]
