c0d9f3fe9cd3c0556174c5beaa9c8d03f91e5407
Nirav <nirav.rajguru@9spl.com>
2022-02-23 19:29:32 +0530

    [#113] add form_approval_assignment_seeder and pending data seeder

---
+++ backend/api/v1/v1_data/management/commands/form_approval_assignment_seeder.py
@@ -0,0 +1,45 @@
+from django.core.management import BaseCommand
+from faker import Faker
+
+from api.v1.v1_forms.models import FormApprovalRule, FormApprovalAssignment
+from api.v1.v1_profile.constants import UserRoleTypes
+from api.v1.v1_profile.models import Administration, Access
+from api.v1.v1_users.models import SystemUser
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        FormApprovalAssignment.objects.all().delete()
+        for rule in FormApprovalRule.objects.all():
+            previous = None
+            for level in rule.levels.all().order_by('level'):
+                if previous:
+                    filter_path = '{0}{1}.'.format(previous.path,
+                                                   previous.id)
+                else:
+                    filter_path = '{0}{1}.'.format(
+                        rule.administration.path,
+                        rule.administration.id)
+                administration = Administration.objects.filter(
+                    path__startswith=filter_path,
+                    level=level).first()
+                previous = administration
+                user = SystemUser.objects.filter(
+                    user_access__administration=administration).first()
+                if not user:
+                    fake = Faker()
+                    profile = fake.profile()
+                    name = profile.get("name").split(" ")
+                    user = SystemUser.objects.create_user(
+                        email=profile.get("mail"),
+                        first_name=name[0],
+                        last_name=name[1])
+                    Access.objects.create(user=user,
+                                          role=UserRoleTypes.approver,
+                                          administration=administration)
+                FormApprovalAssignment.objects.create(
+                    form=rule.form,
+                    administration=administration,
+                    user=user
+                )
