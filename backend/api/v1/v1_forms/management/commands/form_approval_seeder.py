d6525acda10baa6fe5d51a02e48159d332828e97
dedenbangkit <mail@dedenbangkit.com>
2022-02-24 13:28:24 +0700

    [#113] Fix form approval seeder

--- backend/api/v1/v1_forms/management/commands/form_approval_seeder.py
+++ backend/api/v1/v1_forms/management/commands/form_approval_seeder.py
@@ -3,6 +3,7 @@ import random
 from django.core.management import BaseCommand
 
 from api.v1.v1_forms.models import Forms, FormApprovalRule
+from api.v1.v1_forms.constants import FormTypes
 from api.v1.v1_profile.models import Access, Levels
 
 
@@ -10,15 +11,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         FormApprovalRule.objects.all().delete()
         for form in Forms.objects.all():
-            for user in Access.objects.filter(
-                    administration__level=Levels.objects.filter(
-                        level=1).first()).distinct('administration_id'):
-                randoms = Levels.objects.filter(level__gt=1).count()
-                randoms = [n + 1 for n in range(randoms)]
-                limit = random.choices(randoms)
-                levels = Levels.objects.filter(
-                    level__gt=1).order_by('?')[:limit[0]]
-                rule = FormApprovalRule.objects.create(
-                    form=form, administration=user.administration)
-                rule.levels.set(levels)
-                rule.save()
+            if form.type == FormTypes.county:
+                for user in Access.objects.filter(
+                        administration__level=Levels.objects.filter(
+                            level=1).first()).distinct('administration_id'):
+                    randoms = Levels.objects.filter(level__gt=1).count()
+                    randoms = [n + 1 for n in range(randoms)]
+                    limit = random.choices(randoms)
+                    levels = Levels.objects.filter(
+                        level__gt=1).order_by('?')[:limit[0]]
+                    rule = FormApprovalRule.objects.create(
+                        form=form, administration=user.administration)
+                    rule.levels.set(levels)
+                    rule.save()
+            else:
+                for user in Access.objects.filter(
+                        administration__level=Levels.objects.filter(
+                            level=0).first()).distinct('administration_id'):
+                    rule = FormApprovalRule.objects.create(
+                        form=form, administration=user.administration)
+                    rule.levels.set(Levels.objects.filter(level=0))
+                    rule.save()
