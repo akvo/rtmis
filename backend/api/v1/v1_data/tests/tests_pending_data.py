4e0eff790618a2a6b49a16dbec70fff44b72f874
Nirav <nirav.rajguru@9spl.com>
2022-03-14 18:59:00 +0530

    [#184] Fix line length

--- backend/api/v1/v1_data/tests/tests_pending_data.py
+++ backend/api/v1/v1_data/tests/tests_pending_data.py
@@ -184,9 +184,10 @@ class PendingDataTestCase(TestCase):
             PendingDataBatch.objects.first().id), follow=True, **header)
         self.assertEqual(200, response.status_code)
         for summary in response.json():
-            if summary.get('type') in [QuestionTypes.FieldStr.get(
-                    QuestionTypes.option), QuestionTypes.FieldStr.get(
-                QuestionTypes.multiple_option)]:
+            if summary.get('type') in \
+                    [QuestionTypes.FieldStr.get(QuestionTypes.option),
+                     QuestionTypes.FieldStr.get(
+                         QuestionTypes.multiple_option)]:
                 self.assertEqual(['type', 'total'],
                                  list(summary.get('value')[0]))
             else:
