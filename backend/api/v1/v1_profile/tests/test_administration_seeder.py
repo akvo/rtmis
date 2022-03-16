da762d39633e2dfcf9dda9d7e3ea4a71b0d899a3
dedenbangkit <mail@dedenbangkit.com>
2022-03-17 03:21:19 +0700

    [#161] Test improvements, remove tmp

--- backend/api/v1/v1_profile/tests/test_administration_seeder.py
+++ backend/api/v1/v1_profile/tests/test_administration_seeder.py
@@ -2,23 +2,31 @@ from django.test import TestCase
 
 from api.v1.v1_profile.management.commands import administration_seeder
 from api.v1.v1_profile.models import Levels, Administration
+from api.v1.v1_users.serializers import ListAdministrationChildrenSerializer
 
 
 class AdministrationSeederTestCase(TestCase):
-
     def test_administration_seeder_production(self):
         administration_seeder.seed_administration_prod()
         administrator_level = Administration.objects.order_by(
             '-level').values_list('level', flat=True).distinct()
         level_ids = Levels.objects.order_by('-id').values_list('id', flat=True)
         self.assertEqual(list(administrator_level), list(level_ids))
+        children = Administration.objects.filter(level__level=1).all()
+        children = ListAdministrationChildrenSerializer(instance=children,
+                                                        many=True)
         response = self.client.get("/api/v1/administration/1", follow=True)
         self.assertEqual(response.status_code, 200)
-        response = response.json()
-        self.assertEqual(response.get("id"), 1)
-        self.assertEqual(response.get("name"), "Kenya")
-        self.assertIsNone(response.get("parent"))
-        self.assertIsNotNone(response.get("children"))
+        self.assertEqual(
+            {
+                "id": 1,
+                "level": 0,
+                "level_name": "National",
+                "name": "Kenya",
+                "parent": None,
+                "children": children.data,
+                "children_level_name": "County",
+            }, response.json())
 
     def test_administration_seeder_test(self):
         administration_seeder.seed_administration_test()
@@ -26,10 +34,19 @@ class AdministrationSeederTestCase(TestCase):
             '-level').values_list('level', flat=True).distinct()
         level_ids = Levels.objects.order_by('-id').values_list('id', flat=True)
         self.assertEqual(list(administrator_level), list(level_ids))
-        response = self.client.get("/api/v1/administration/1", follow=True)
+        response = self.client.get('/api/v1/administration/1', follow=True)
         self.assertEqual(response.status_code, 200)
-        response = response.json()
-        self.assertEqual(response.get("id"), 1)
-        self.assertEqual(response.get("name"), "Indonesia")
-        self.assertIsNone(response.get("parent"))
-        self.assertIsNotNone(response.get("children"))
+        self.assertEqual(
+            {
+                "id": 1,
+                "level": 0,
+                "level_name": "National",
+                "name": "Indonesia",
+                "parent": None,
+                "children": [{
+                    "id": 2,
+                    "name": "Jakarta",
+                    "parent": 1
+                }],
+                "children_level_name": "County",
+            }, response.json())
