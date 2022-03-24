00b1cccc6015c82a7b57909c1df841deede2e96a
Nirav <nirav.rajguru@9spl.com>
2022-03-24 18:11:21 +0530

    [#223] download list API

--- backend/api/v1/v1_jobs/serializers.py
+++ backend/api/v1/v1_jobs/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 from api.v1.v1_forms.models import Forms
+from api.v1.v1_jobs.constants import JobTypes, JobStatus
+from api.v1.v1_jobs.models import Jobs
 from api.v1.v1_profile.models import Administration
 from utils.custom_serializer_fields import CustomPrimaryKeyRelatedField
 
@@ -15,3 +17,20 @@ class GenerateDownloadRequestSerializer(serializers.Serializer):
         self.fields.get('form_id').queryset = Forms.objects.all()
         self.fields.get(
             'administration_id').queryset = Administration.objects.all()
+
+
+class DownloadListSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+    created_by = serializers.ReadOnlyField(source='user.id')
+
+    def get_type(self, instance):
+        return JobTypes.FieldStr.get(instance.type)
+
+    def get_status(self, instance):
+        return JobStatus.FieldStr.get(instance.status)
+
+    class Meta:
+        model = Jobs
+        fields = ['id', 'type', 'status', 'info', 'attempt', 'created_by',
+                  'created', 'result', 'available']
