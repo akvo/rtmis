c8cd34384f0f82091e6b83a41ea55bc3241ff429
wayangalihpratama <wgprtm@gmail.com>
2022-04-19 12:59:16 +0800

    [#278] Create endpoint to show email template by type

--- backend/utils/email_helper.py
+++ backend/utils/email_helper.py
@@ -2,14 +2,50 @@ from pathlib import Path
 
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
-
+from rest_framework import serializers
+from utils.custom_serializer_fields import CustomChoiceField
 from rtmis.settings import EMAIL_FROM
 
 
-def send_email(context: dict, template, path=None, content_type=None):
+class EmailTypes:
+    register = 'register'
+    data_approval = 'data_approval'
+    data_rejection = 'data_rejection'
+
+    FieldStr = {
+        register: 'register',
+        data_approval: 'data_approval',
+        data_rejection: 'data_rejection'
+    }
+
+
+class ListEmailTypeRequestSerializer(serializers.Serializer):
+    type = CustomChoiceField(choices=list(EmailTypes.FieldStr.keys()),
+                             required=True)
+
+
+def send_email(context: dict, path=None, content_type=None,
+               send=True, type=None):
+    context.update({
+        "webdomain": "https://rtmis.akvotest.org",
+        "logo": "https://rtmis.akvotest.org/logo.png",
+        "site_name": "MOH"
+    })
+
+    if type == EmailTypes.register:
+        context.update({
+            "body": '''Welcome to the lore Epsom door sit amen
+                some Descriptive welcome copy goes here''',
+            "image": "https://rtmis.akvotest.org/email-icons/check-circle.png",
+            "success_text": "Successfully Registered",
+            "message_list": ["JMP/SDG Status",
+                             "CLTS Progress",
+                             "Water Infrastructure"]
+        })
+
     try:
-        email_html_message = render_to_string(
-            "email/{0}".format(template), context)
+
+        email_html_message = render_to_string("email/main.html", context)
         msg = EmailMultiAlternatives(
             context.get('subject'),
             'Email plain text',
@@ -20,6 +56,9 @@ def send_email(context: dict, template, path=None, content_type=None):
         if path:
             msg.attach(Path(path).name, open(path).read(),
                        content_type)
-        msg.send()
+        if send:
+            msg.send()
+        if not send:
+            return email_html_message
     except Exception as ex:
         print(ex)
