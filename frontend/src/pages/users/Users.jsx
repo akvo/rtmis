68260ae896188ca00d2b7faaae0b79a5fbb245ce
abhishek <abhishekps6282@gmail.com>
2022-04-26 18:47:02 +0530

    [294] result summary for table added

--- frontend/src/pages/users/Users.jsx
+++ frontend/src/pages/users/Users.jsx
@@ -169,6 +169,8 @@ const Users = () => {
             current: currentPage,
             total: totalCount,
             pageSize: 10,
+            showTotal: (total, range) =>
+              `Results: ${range[0]} - ${range[1]} of ${total} users`,
           }}
           rowKey="id"
           expandable={{
