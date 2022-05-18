e3d295b3143416a8ec0a87a31665a1a39ff72ba0
wayangalihpratama <wgprtm@gmail.com>
2022-05-18 17:49:23 +0800

    [#326] Finalize user_activity middleware

--- backend/middleware/user_activity.py
+++ backend/middleware/user_activity.py
@@ -11,23 +11,11 @@ class UserActivity(object):
     def __call__(self, request):
         response = self.get_response(request)
         try:
+            # auth_header = request.headers.get('Authorization')
             # get user detail
             user = SystemUser.objects.get(email=request.user)
-            # calculate last activity
-            now = timezone.now()
-            last_active = user.last_login
-            time_diff_hours = None
-            if last_active:
-                time_diff = now - last_active
-                time_diff_hours = time_diff.total_seconds()
-                # / 3600
-            if time_diff_hours and time_diff_hours >= 2:
-                # revoke after 2 hours inactivity
-                # reset last active
-                user.last_login = None
-            else:
-                # update last login here
-                user.last_login = timezone.now()
+            # update last login here
+            user.last_login = timezone.now()
             user.save()
             return response
         except SystemUser.DoesNotExist:
