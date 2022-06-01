098724315b7ae6b24a9080e8d493822e57a23122
Sanju Sunny <mantiz8x@gmail.com>
2022-06-01 20:15:23 +0530

    [#433] Inline Editor for Submitter - WIP

--- frontend/src/pages/data-uploads/DataUploads.jsx
+++ frontend/src/pages/data-uploads/DataUploads.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./style.scss";
-import { Row, Col, Card, Divider, Table, Tabs, Button } from "antd";
+import { Card, Divider, Table, Tabs } from "antd";
 import { Breadcrumbs } from "../../components";
-import { Link } from "react-router-dom";
 import { PlusSquareOutlined, CloseSquareOutlined } from "@ant-design/icons";
-import { api, store } from "../../lib";
+import { api } from "../../lib";
 import { columnsApproval } from "./";
-import ApprovalDetails from "./ApprovalDetail";
+import UploadDetail from "./UploadDetail";
 
 const columns = [...columnsApproval, Table.EXPAND_COLUMN];
 
@@ -16,31 +15,25 @@ const pagePath = [
     link: "/control-center",
   },
   {
-    title: "Approvals",
+    title: "Data Uploads",
   },
 ];
 const { TabPane } = Tabs;
 
-const Approvals = () => {
+const DataUploads = () => {
   const [batches, setBatches] = useState([]);
-  const [approvalTab, setApprovalTab] = useState("my-pending");
+  const [approvalTab, setApprovalTab] = useState("pending-submission");
   const [totalCount, setTotalCount] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const [expandedKeys, setExpandedKeys] = useState([]);
-  const [role, setRole] = useState(null);
   const [loading, setLoading] = useState(true);
   const [reload, setReload] = useState(0);
-  const { user } = store.useState((state) => state);
-
-  useEffect(() => {
-    setRole(user?.role?.id);
-  }, [user]);
 
   useEffect(() => {
     setLoading(true);
     let url = `/form-pending-batch/?page=${currentPage}`;
-    if (approvalTab === "subordinate") {
-      url = `${url}&subordinate=true`;
+    if (approvalTab === "pending-approval") {
+      url = `${url}&pending-approval=true`;
     }
     if (approvalTab === "approved") {
       url = `${url}&approved=true`;
@@ -64,26 +57,15 @@ const Approvals = () => {
 
   return (
     <div id="approvals">
-      <Row justify="space-between">
-        <Col>
-          <Breadcrumbs pagePath={pagePath} />
-        </Col>
-        <Col>
-          {(role === 1 || role === 2) && (
-            <Link to={role === 1 ? "/questionnaires" : "/questionnaires/admin"}>
-              <Button type="primary">Manage Questionnaire Approval</Button>
-            </Link>
-          )}
-        </Col>
-      </Row>
+      <Breadcrumbs pagePath={pagePath} />
       <Divider />
       <Card
         style={{ padding: 0, minHeight: "40vh" }}
         bodyStyle={{ padding: 30 }}
       >
         <Tabs defaultActiveKey={approvalTab} onChange={setApprovalTab}>
-          <TabPane tab="My Pending Approvals" key="my-pending"></TabPane>
-          <TabPane tab="Subordinates Approvals" key="subordinate"></TabPane>
+          <TabPane tab="Pending Submission" key="pending-submission"></TabPane>
+          <TabPane tab="Pending Approval" key="pending-approval"></TabPane>
           <TabPane tab="Approved" key="approved"></TabPane>
         </Tabs>
         <Table
@@ -103,9 +85,9 @@ const Approvals = () => {
           expandable={{
             expandedRowRender: (record) => {
               return (
-                <ApprovalDetails
+                <UploadDetail
                   record={record}
-                  approve={approvalTab === "my-pending"}
+                  approve={approvalTab === "pending-submission"}
                   setReload={setReload}
                   expandedParentKeys={expandedKeys}
                   setExpandedParentKeys={setExpandedKeys}
@@ -140,4 +122,4 @@ const Approvals = () => {
   );
 };
 
-export default React.memo(Approvals);
+export default React.memo(DataUploads);
