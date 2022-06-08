736655f3da66efbbbc129f800cb1c332b5127876
dedenbangkit <mail@dedenbangkit.com>
2022-06-08 22:52:00 +0700

    Update gitstat command

--- gitstat.sh
+++ gitstat.sh
@@ -6,21 +6,29 @@
 get_contributor() {
     git log --format='%aN' \
         | grep -v "root" \
+        | grep -v "abhishek_p_s" \
         | grep -v "Deden Bangkit" \
         | grep -v "nirav9spl" \
         | grep -v "bhavin" \
         | sort -u
 }
+
+contributors=$(get_contributor)
+
+if [ $# -eq 1 ]; then
+    contributors=$(echo "${contributors}" | grep "$1")
+fi
+
 while read -r contributor; do
     echo ""
     echo "### Contributor: ${contributor}"
     git log --author="${contributor}" --pretty=tformat: --numstat \
         | grep -v "migrations" \
         | grep -v "__snapshots__" \
-        | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "**Added Lines: %s, Removed Lines: %s, Total Lines: %s**\n", add, subs, loc }' -
+        | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "Added Lines: %s, Removed Lines: %s, Total Lines: %s\n", add, subs, loc }' -
     echo ""
-    git --no-pager log --reverse --author="${contributor}" --pretty=format:"- **%Cblue%cd%Creset** %Cred%cn%Creset %s" --date=format:'%d %h %H:%M'
+    git --no-pager log --reverse --author="${contributor}" --no-merges --pretty=format:"- [%Cblue%cd%Creset] %Cred%cn%Creset %s" --date=format:'%d %h %H:%M'
     echo ""
     echo ""
     echo "----------------------------------------------------------------"
-done < <(get_contributor)
+done < <(echo "${contributors}")
