82e7893a9bd9805195af440f1d60bdedc806b605
wayangalihpratama <wgprtm@gmail.com>
2022-06-28 18:33:55 +0800

    [#520] Add about text and uncomment highlights visualisation

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import "./style.scss";
-// import { Tabs } from "antd";
-// import { HomeDataChart, HomeAdministrationChart } from "../../components";
-// const { TabPane } = Tabs;
+import { Tabs } from "antd";
+import { HomeDataChart, HomeAdministrationChart } from "../../components";
+const { TabPane } = Tabs;
 
 const Home = () => {
-  // const { highlights } = window;
+  const { highlights } = window;
 
   return (
     <div id="home">
-      {/* <div className="highlights">
+      <div className="home-even about">
+        <h1>About RUSH</h1>
+        <p>
+          The Kenya Rural Urban Sanitation and Hygiene (RUSH) platform is a
+          real-time monitoring and information system owned by the Ministry of
+          Health. The platform aggregates quantitative and qualitative data from
+          county and national levels and facilitates data analysis, report
+          generation and visualizations.
+        </p>
+      </div>
+      <div className="home-odd about">
+        <h1>About the data</h1>
+        <p>
+          All the data contained in the RUSH platform is aggregated from both
+          primary and secondary data sources from the 47 counties in Kenya. The
+          data is updated on a monthly basis.
+        </p>
+      </div>
+      <div className="home-even highlights">
         <h1>Highlights</h1>
         <div className="body">
           <Tabs defaultActiveKey="1" centered>
@@ -37,7 +55,7 @@ const Home = () => {
             ))}
           </Tabs>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
