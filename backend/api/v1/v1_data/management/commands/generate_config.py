b3827ae0be0b4b5d0e1061c7c4f2db5b8454c434
dedenbangkit <mail@dedenbangkit.com>
2022-06-30 02:59:10 +0700

    [#532] Add administration to config.js

--- backend/api/v1/v1_data/management/commands/generate_config.py
+++ backend/api/v1/v1_data/management/commands/generate_config.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from django.core.management import BaseCommand
 from jsmin import jsmin
@@ -9,13 +10,20 @@ from api.v1.v1_profile.models import Levels, Administration
 
 
 class Command(BaseCommand):
-
     def handle(self, *args, **options):
         # fetch all administrations
         all_administrations = {}
+        adm = []
         administrations = Administration.objects.all()
         for a in administrations:
-            all_administrations.update({a.name: a.id})
+            all_administrations.update({a.name: {"id": a.id, "path": a.path}})
+            adm.append({
+                "id": a.id,
+                "parent": a.parent_id,
+                "name": a.name,
+                "path": a.path,
+                "full_name": a.administration_column
+            })
         # add administration id to topojson
         topojson = open("source/kenya.topojson").read()
         topojson = json.loads(topojson)
@@ -24,7 +32,7 @@ class Command(BaseCommand):
             key = obj['properties']['NAME_3']
             find_id = 0
             if key in all_administrations:
-                find_id = all_administrations[key]
+                find_id = all_administrations[key].get("id")
             obj['properties'].update({"SHAPE_ADMIN_ID": find_id})
             geometris_with_id.append(obj)
         topojson['objects']['kenya']['geometries'] = geometris_with_id
@@ -32,6 +40,10 @@ class Command(BaseCommand):
         new_topojson_file = "source/kenya_with_admin_id.topojson"
         with open(new_topojson_file, "w") as outfile:
             json.dump(topojson, outfile)
+        # write administration_file
+        administration_json = "source/administration.json"
+        with open(administration_json, "w") as outfile:
+            json.dump(adm, outfile)
 
         # write config
         config_file = jsmin(open("source/config/config.js").read())
@@ -52,9 +64,12 @@ class Command(BaseCommand):
                 'type_text': FormTypes.FieldStr.get(form.type),
             })
         min_config = jsmin("".join([
-            "var topojson=", open(new_topojson_file).read(), ";",
-            "var levels=", json.dumps(levels), ";",
-            "var forms=", json.dumps(forms), ";",
-            config_file
+            "var dbadm=",
+            open(administration_json).read(), ";",
+            "var topojson=",
+            open(new_topojson_file).read(), ";", "var levels=",
+            json.dumps(levels), ";", "var forms=",
+            json.dumps(forms), ";", config_file
         ]))
         open("source/config/config.min.js", 'w').write(min_config)
+        os.remove(administration_json)
