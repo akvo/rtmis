ed85cbf7a07738b2b42cb53d02cd19cbb4049243
wayangalihpratama <wgprtm@gmail.com>
2022-07-01 10:20:44 +0800

    [#529] When add new user as approver, record this approver to approval assignment table

--- backend/api/v1/v1_users/views.py
+++ backend/api/v1/v1_users/views.py
@@ -267,24 +267,37 @@ def add_user(request, version):
         return Response(
             {'message': validate_serializers_message(serializer.errors)},
             status=status.HTTP_400_BAD_REQUEST)
-    # Check if form id x in y administration has approver assignment already,
-    # send a message to FE 404
-    form_approval_assignment = FormApprovalAssignment.objects.filter(
-        form__in=serializer.validated_data.get('forms'),
-        administration=serializer.validated_data.get('administration')
-    ).distinct('form', 'administration').all()
-    if form_approval_assignment:
-        message_detail = ["{0} - {1}".format(
-            fa.form.name, fa.administration.name
-        ) for fa in form_approval_assignment]
-        return Response(
-            {'message': '''There are existing approvers for {0}.
-             Please update the setup in manage validation tree or remove these
-             forms for the current user'''.format(", ".join(message_detail))},
-            status=status.HTTP_404_NOT_FOUND)
-    # TODO:: need to add user value to approval assignment table
+
+    # when add new user as approver
+    if serializer.validated_data.get('role') == UserRoleTypes.approver:
+        # Check if form id x in y administration has approver assignment
+        # send a message to FE 404
+        form_approval_assignment = FormApprovalAssignment.objects.filter(
+            form__in=serializer.validated_data.get('forms'),
+            administration=serializer.validated_data.get('administration')
+        ).distinct('form', 'administration').all()
+        if form_approval_assignment:
+            message_detail = ["{0} - {1}".format(
+                fa.form.name, fa.administration.name
+            ) for fa in form_approval_assignment]
+            return Response(
+                {'message': '''There are existing approvers for {0}.
+                Please update the setup in manage validation tree or remove
+                these forms for the current user'''.format(
+                    ", ".join(message_detail))},
+                status=status.HTTP_404_NOT_FOUND)
 
     user = serializer.save()
+    # when add new user as approver
+    if serializer.validated_data.get('role') == UserRoleTypes.approver:
+        # Add user value to approval assignment table
+        form_approval_obj = [FormApprovalAssignment(
+            form=fr,
+            administration=serializer.validated_data.get('administration'),
+            user=user
+        ) for fr in serializer.validated_data.get('forms')]
+        FormApprovalAssignment.objects.bulk_create(form_approval_obj)
+
     url = f"{webdomain}/login/{signing.dumps(user.pk)}"
     user = Access.objects.filter(user=user.pk).first()
     admin = Access.objects.filter(user=request.user.pk).first()
