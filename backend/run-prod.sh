5fd686f737b5c8b6842e48d874e644b42177d0be
dedenbangkit <mail@dedenbangkit.com>
2022-07-04 15:14:25 +0700

    Run without gunicorn

--- backend/run-prod.sh
+++ backend/run-prod.sh
@@ -6,4 +6,5 @@ set -eu
 python manage.py migrate
 python manage.py generate_config
 
-gunicorn rtmis.wsgi --max-requests 200 --workers 6 --timeout 300 --bind 0.0.0.0:8000
+# gunicorn rtmis.wsgi --max-requests 200 --workers 6 --timeout 300 --bind 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
