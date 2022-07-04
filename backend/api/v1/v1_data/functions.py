2be471ca07f9d70a9cb3a57265ce4fba5e340762
dedenbangkit <mail@dedenbangkit.com>
2022-07-04 15:35:39 +0700

    [#528] Fix reverted cache

--- backend/api/v1/v1_data/functions.py
+++ backend/api/v1/v1_data/functions.py
@@ -1,8 +1,8 @@
 import re
 import json
+from pathlib import Path
 from datetime import datetime
 from django.db import transaction, connection
-from django.core.cache import cache
 from django.http import HttpResponse
 from rtmis.settings import CACHE_FOLDER
 
@@ -14,13 +14,15 @@ def refresh_materialized_data():
             REFRESH MATERIALIZED VIEW view_data_options;""")
 
 
-def get_cache(name, as_middleware=True):
+def get_cache(name):
     name = re.sub(r'[\W_]+', '_', name)
     today = datetime.now().strftime("%Y%m%d")
     cache_name = f"{CACHE_FOLDER}{today}-{name}.json"
-    data = cache.get(cache_name)
-    if data:
-        return HttpResponse(cache, content_type="application/json;")
+    if Path(cache_name).exists():
+        with open(cache_name, 'r') as cache_file:
+            return HttpResponse(
+                    cache_file,
+                    content_type="application/json;")
     return None
 
 
@@ -28,7 +30,6 @@ def create_cache(name, resp):
     name = re.sub(r'[\W_]+', '_', name)
     today = datetime.now().strftime("%Y%m%d")
     cache_name = f"{CACHE_FOLDER}{today}-{name}.json"
-    cache.add(cache_name, resp)
     json_cache = json.dumps(resp, separators=(',', ":"))
     with open(cache_name, "w") as outfile:
         outfile.write(json_cache)
