2beee007e142f4726f986d0e23a493b015a35356
dedenbangkit <mail@dedenbangkit.com>
2022-07-04 15:03:25 +0700

    Remove SIGTERM log

--- backend/run-prod.sh
+++ backend/run-prod.sh
@@ -6,19 +6,4 @@ set -eu
 python manage.py migrate
 python manage.py generate_config
 
-function log {
-   echo "$(date +"%T") - START INFO - $*"
-}
-
-_term() {
-  echo "Caught SIGTERM signal!"
-  kill -TERM "$child" 2>/dev/null
-}
-
-trap _term SIGTERM
-
-log Starting gunicorn in background
-gunicorn rtmis.wsgi --max-requests 200 --workers 6 --timeout 300 --bind 0.0.0.0:8000 &
-
-child=$!
-wait "$child"
+gunicorn rtmis.wsgi --max-requests 200 --workers 6 --timeout 300 --bind 0.0.0.0:8000
