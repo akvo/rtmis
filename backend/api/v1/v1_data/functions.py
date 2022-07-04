ab81e586f33b572891d7edc620c44a6d4321419a
dedenbangkit <mail@dedenbangkit.com>
2022-07-04 19:21:44 +0700

    [#528] Message pack cache test

--- backend/api/v1/v1_data/functions.py
+++ backend/api/v1/v1_data/functions.py
@@ -1,4 +1,5 @@
 import re
+import os
 from pathlib import Path
 import msgpack
 from datetime import datetime
@@ -17,9 +18,9 @@ def refresh_materialized_data():
 def get_cache(name):
     name = re.sub(r'[\W_]+', '_', name)
     today = datetime.now().strftime("%Y%m%d")
-    cache_name = f"{today}-{name}"
+    cache_name = f"{CACHE_FOLDER}{today}-{name}.msgpack"
     if Path(cache_name).exists():
-        with open(f"{CACHE_FOLDER}{cache_name}.msgpack", "rb") as data_file:
+        with open(cache_name, "rb") as data_file:
             byte_data = data_file.read()
             data_loaded = msgpack.unpackb(byte_data)
             return HttpResponse(data_loaded, content_type="application/json;")
@@ -30,6 +31,8 @@ def create_cache(name, resp):
     name = re.sub(r'[\W_]+', '_', name)
     today = datetime.now().strftime("%Y%m%d")
     cache_name = f"{today}-{name}"
+    if not Path(CACHE_FOLDER).exists():
+        os.mkdir(CACHE_FOLDER)
     with open(f"{CACHE_FOLDER}{cache_name}.msgpack", "wb") as outfile:
         packed = msgpack.packb(resp)
         outfile.write(packed)
