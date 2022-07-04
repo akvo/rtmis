afc6280a9b266952fb8ae335da58b240adba8656
dedenbangkit <mail@dedenbangkit.com>
2022-07-05 00:49:30 +0700

    [#528] Fix cache issue 🔥 🔥

--- backend/api/v1/v1_data/functions.py
+++ backend/api/v1/v1_data/functions.py
@@ -1,11 +1,7 @@
 import re
-import os
-from pathlib import Path
-import msgpack
+from django.core.cache import cache
 from datetime import datetime
 from django.db import transaction, connection
-from django.http import HttpResponse
-from rtmis.settings import CACHE_FOLDER
 
 
 @transaction.atomic
@@ -20,12 +16,10 @@ def refresh_materialized_data():
 def get_cache(name):
     name = re.sub(r'[\W_]+', '_', name)
     today = datetime.now().strftime("%Y%m%d")
-    cache_name = f"{CACHE_FOLDER}{today}-{name}.msgpack"
-    if Path(cache_name).exists():
-        with open(cache_name, "rb") as data_file:
-            byte_data = data_file.read()
-            data_loaded = msgpack.unpackb(byte_data)
-            return HttpResponse(data_loaded, content_type="application/json;")
+    cache_name = f"{today}-{name}"
+    data = cache.get(cache_name)
+    if data:
+        return data
     return None
 
 
@@ -33,8 +27,4 @@ def create_cache(name, resp):
     name = re.sub(r'[\W_]+', '_', name)
     today = datetime.now().strftime("%Y%m%d")
     cache_name = f"{today}-{name}"
-    if not Path(CACHE_FOLDER).exists():
-        os.mkdir(CACHE_FOLDER)
-    with open(f"{CACHE_FOLDER}{cache_name}.msgpack", "wb") as outfile:
-        packed = msgpack.packb(resp)
-        outfile.write(packed)
+    cache.add(cache_name, resp)
