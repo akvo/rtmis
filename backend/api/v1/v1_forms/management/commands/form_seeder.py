e3c9ad018bbca3b0c01e8b94ba66e96f9b2084d2
dedenbangkit <mail@dedenbangkit.com>
2022-07-05 22:08:04 +0700

    [#548] Modify form seeder with update method

--- backend/api/v1/v1_forms/management/commands/form_seeder.py
+++ backend/api/v1/v1_forms/management/commands/form_seeder.py
@@ -5,8 +5,10 @@ from django.core.management import BaseCommand
 
 from api.v1.v1_forms.constants import QuestionTypes
 from api.v1.v1_profile.constants import UserRoleTypes
-from api.v1.v1_forms.models import Forms, UserForms, QuestionGroup
-from api.v1.v1_forms.models import Questions, QuestionOptions
+from api.v1.v1_forms.models import Forms, UserForms
+from api.v1.v1_forms.models import QuestionGroup as QG
+from api.v1.v1_forms.models import Questions
+from api.v1.v1_forms.models import QuestionOptions as QO
 from api.v1.v1_profile.models import Access
 
 
@@ -29,7 +31,7 @@ class Command(BaseCommand):
         source_files = list(
             filter(lambda x: "example" in x
                    if test else "example" not in x, source_files))
-        Forms.objects.all().delete()
+        # Forms.objects.all().delete()
         for source in source_files:
             json_form = open(source, 'r')
             json_form = json.load(json_form)
@@ -44,15 +46,23 @@ class Command(BaseCommand):
                 role=UserRoleTypes.super_admin).all()
             if created:
                 form.save()
+            if created and not test:
+                self.stdout.write(f"Form Created | {form.name}")
             for user in users:
                 user_form = UserForms(form=form, user_id=user.user_id)
                 user_form.save()
             for qg in json_form["question_groups"]:
-                question_group = QuestionGroup(name=qg["question_group"],
-                                               form=form)
-                question_group.save()
+                question_group, created = QG.objects.update_or_create(
+                    name=qg["question_group"],
+                    form=form,
+                    defaults={
+                        "name": qg["question_group"],
+                        "form": form
+                    })
+                if created:
+                    question_group.save()
                 for qi, q in enumerate(qg["questions"]):
-                    question = Questions.objects.create(
+                    question, created = Questions.objects.update_or_create(
                         id=q.get("id"),
                         name=q["question"],
                         text=q["question"],
@@ -63,11 +73,25 @@ class Command(BaseCommand):
                         rule=q.get("rule"),
                         required=q.get("required"),
                         dependency=q.get("dependency"),
-                        type=getattr(QuestionTypes, q["type"]))
+                        type=getattr(QuestionTypes, q["type"]),
+                        defaults={
+                            "name": q["question"],
+                            "text": q["question"],
+                            "order": qi,
+                            "meta": q.get("meta"),
+                            "form": form,
+                            "question_group": question_group
+                        })
+                    if created:
+                        question.save()
                     if q.get("options"):
-                        question.question_question_options.bulk_create([
-                            QuestionOptions(question=question, name=o["name"])
-                            for o in q["options"]
-                        ])
-            if not test:
-                self.stdout.write(f"Form Created | {form.name}")
+                        for o in q.get("options"):
+                            option, created = QO.objects.update_or_create(
+                                name=o["name"],
+                                question=question,
+                                defaults={
+                                    "name": o["name"],
+                                    "question": question
+                                })
+                            if created:
+                                option.save()
