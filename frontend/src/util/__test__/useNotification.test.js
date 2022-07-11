bacb6b27f828db713898126d5c03e116bf7d765c
lightme-fan <fanilo.and@onja.org>
2022-07-11 09:54:43 +0300

    [#556] - update useNotification test

--- frontend/src/util/__test__/useNotification.test.js
+++ frontend/src/util/__test__/useNotification.test.js
@@ -1,24 +1,42 @@
 import { useNotification } from "../hooks";
 import { render } from "@testing-library/react";
 import { useEffect } from "react";
+import { message } from "antd";
 
-const TestNotification = () => {
+const TestNotification = ({ notificationBody }) => {
   const { notify } = useNotification();
 
   useEffect(() => {
-    notify({
-      type: "error",
-      message: "Not Found",
-    });
-  }, [notify]);
+    notify(notificationBody);
+  }, [notify, notificationBody]);
   return <div>Message: Not Found</div>;
 };
 
-jest.mock("antd");
+jest.mock("antd", () => ({ message: { open: jest.fn(), config: jest.fn() } }));
+
+const notify = {
+  content: "Not Found",
+  style: {
+    marginRight: 20,
+    textAlign: "right",
+  },
+  type: "error",
+};
 
 describe("util/hooks", () => {
-  test("test useNotification", () => {
-    const { asFragment } = render(<TestNotification />);
-    expect(asFragment()).toMatchSnapshot();
+  test("test Notification that shows error type with default style", () => {
+    const notificationBody = {
+      type: "error",
+      message: "Not Found",
+    };
+    render(<TestNotification notificationBody={notificationBody} />);
+    expect(message.open).toHaveBeenCalledTimes(1);
+    expect(message.open).toHaveBeenCalledWith(notify);
+    expect(message.open).toMatchSnapshot();
+  });
+  test("shows Notification when no body is passed", () => {
+    const notificationBody = {};
+    render(<TestNotification notificationBody={notificationBody} />);
+    expect(message.open).toHaveBeenCalledTimes(1);
   });
 });
