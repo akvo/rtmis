2352e12e89606b3a856684bc39886157877165da
wayangalihpratama <wgprtm@gmail.com>
2022-07-13 15:32:33 +0800

    [#560] Dynamically update counties card title based on administration

--- frontend/src/pages/dashboard/components/CardVisual.js
+++ frontend/src/pages/dashboard/components/CardVisual.js
@@ -1,10 +1,25 @@
 import React, { useMemo } from "react";
 import { Col, Card, Image } from "antd";
-import { get, sum } from "lodash";
+import { store } from "../../../lib";
+import { get, sum, takeRight } from "lodash";
 
 const CardVisual = ({ cardConfig }) => {
+  const { administration } = store.useState((s) => s);
+  const currentAdministration = takeRight(administration)?.[0];
   const { title, type, path, calc, span, data, index, color, icon } =
     cardConfig;
+
+  const admLevelName = useMemo(() => {
+    const { level } = currentAdministration;
+    let name = "Counties";
+    if (level === 1) {
+      name = "Sub-Counties";
+    }
+    if (level === 2) {
+      name = "Wards";
+    }
+    return name;
+  }, [currentAdministration]);
 
   const renderData = useMemo(() => {
     if (!path || !data.length) {
@@ -54,7 +69,9 @@ const CardVisual = ({ cardConfig }) => {
             alt={icon}
           />
         )}
-        <h3 className={icon ? "with-icon" : ""}>{renderData?.title}</h3>
+        <h3 className={icon ? "with-icon" : ""}>
+          {renderData?.title?.replace("##administration_level##", admLevelName)}
+        </h3>
         <h1 className={icon ? "with-icon" : ""}>{renderData?.value}</h1>
       </Card>
     </Col>
