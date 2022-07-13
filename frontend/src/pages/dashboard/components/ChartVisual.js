a01487e6f0d9e98790d1e0d55cd83f906d7e9b86
wayangalihpratama <wgprtm@gmail.com>
2022-07-13 18:33:13 +0800

    [#560] Render example trend chart

--- frontend/src/pages/dashboard/components/ChartVisual.js
+++ frontend/src/pages/dashboard/components/ChartVisual.js
@@ -1,14 +1,24 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Col, Card } from "antd";
 import { get, capitalize, chain, groupBy, sumBy } from "lodash";
 import { Chart } from "../../../components";
 
+const exampleTrendChartData = [
+  { name: "Mon", value: 820 },
+  { name: "Tue", value: 932 },
+  { name: "Wed", value: 901 },
+  { name: "Thu", value: 934 },
+  { name: "Fri", value: 1290 },
+  { name: "Sat", value: 1330 },
+  { name: "Sun", value: 1320 },
+];
+
 const ChartVisual = ({ chartConfig }) => {
-  // TODO:: set api config for trend chart
-  const { title, type, span, data, index, path } = chartConfig;
+  const { title, type, span, data, index, path, api } = chartConfig;
+  const [chartDataApi, setChartDatApi] = useState([]);
 
   const chartData = useMemo(() => {
-    if (!path && !data.length) {
+    if (!path && api && !data.length) {
       return [];
     }
     const transform = data
@@ -30,14 +40,45 @@ const ChartVisual = ({ chartConfig }) => {
         value: sumBy(g, "value"),
       }))
       .value();
-  }, [data, path]);
+  }, [data, path, api]);
+
+  useEffect(() => {
+    if (api && !path) {
+      setChartDatApi(exampleTrendChartData);
+    }
+  }, [api, path]);
 
   return (
     <Col key={`col-${type}-${index}`} span={span}>
       <Card>
         <h3>{title}</h3>
-        {!path ? (
-          <h4>No data</h4>
+        {!path && api ? (
+          <Chart
+            height={50 * chartDataApi.length + 188}
+            type="LINEAREA"
+            data={chartDataApi}
+            wrapper={false}
+            horizontal={true}
+            loading={!chartDataApi.length}
+            series={{
+              left: "10%",
+            }}
+            legend={{
+              top: "middle",
+              left: "65%",
+              right: "right",
+              orient: "vertical",
+              itemGap: 12,
+              textStyle: {
+                fontWeight: "normal",
+                fontSize: 12,
+              },
+            }}
+            grid={{
+              top: 90,
+              left: 120,
+            }}
+          />
         ) : (
           <Chart
             height={50 * chartData.length + 188}
@@ -46,7 +87,6 @@ const ChartVisual = ({ chartConfig }) => {
             wrapper={false}
             horizontal={true}
             loading={!chartData.length}
-            // extra={{ color: chartColors, animation: next === index + 1 }}
             series={{
               left: "10%",
             }}
