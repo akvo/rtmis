5d4d97c353b6344b2323fbe22b8f00c86fe89188
wayangalihpratama <wgprtm@gmail.com>
2022-07-29 19:54:33 +0800

    [#637] Show rule & extra value on form/web API result

--- backend/api/v1/v1_forms/serializers.py
+++ backend/api/v1/v1_forms/serializers.py
@@ -39,6 +39,8 @@ class ListQuestionSerializer(serializers.ModelSerializer):
     center = serializers.SerializerMethodField()
     api = serializers.SerializerMethodField()
     name = serializers.SerializerMethodField()
+    rule = serializers.SerializerMethodField()
+    extra = serializers.SerializerMethodField()
 
     @extend_schema_field(ListOptionSerializer(many=True))
     def get_option(self, instance: Questions):
@@ -93,6 +95,24 @@ class ListQuestionSerializer(serializers.ModelSerializer):
     def get_name(self, instance: Questions):
         return instance.text
 
+    @extend_schema_field(
+        inline_serializer('QuestionRuleFormat',
+                          fields={
+                              'min': serializers.FloatField(),
+                              'max': serializers.FloatField(),
+                              'allowDecimal': serializers.BooleanField(),
+                          }))
+    def get_rule(self, instance: Questions):
+        return instance.rule
+
+    @extend_schema_field(
+        inline_serializer('QuestionExtraFormat',
+                          fields={
+                              'allowOther': serializers.BooleanField()
+                          }))
+    def get_extra(self, instance: Questions):
+        return instance.extra
+
     def to_representation(self, instance):
         result = super(ListQuestionSerializer,
                        self).to_representation(instance)
@@ -103,7 +123,7 @@ class ListQuestionSerializer(serializers.ModelSerializer):
         model = Questions
         fields = [
             'id', 'name', 'order', 'type', 'required', 'dependency', 'option',
-            'center', 'api', 'meta'
+            'center', 'api', 'meta', 'rule', 'extra'
         ]
 
 
