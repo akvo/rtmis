9f79e2b9b21fdafb65b1f643c006fe221fc45077
wayangalihpratama <wgprtm@gmail.com>
2022-08-02 18:30:42 +0800

    [#652] Modify score/color config, legend ordering & chart data ordering by jmp score

--- frontend/src/pages/dashboard/components/ChartVisual.jsx
+++ frontend/src/pages/dashboard/components/ChartVisual.jsx
@@ -1,7 +1,7 @@
 import React, { useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Row, Col, Card, Switch, Space } from "antd";
-import { get, capitalize, chain, groupBy, sumBy, orderBy } from "lodash";
+import { Row, Col, Card, Switch, Space, Checkbox } from "antd";
+import { get, capitalize, chain, groupBy, sumBy, orderBy, isNil } from "lodash";
 import { Chart } from "../../../components";
 import { jmpColorScore } from "../../../lib";
 
@@ -14,9 +14,14 @@ const ChartVisual = ({ chartConfig, loading }) => {
       ? String(path).replace("data", formId)
       : String(path).replace("jmp", formId);
   const [isStack, setIsStack] = useState(false);
+  const [showEmpty, setShowEmpty] = useState(false);
   const showSwitcher = calc === "jmp";
 
   const chartData = useMemo(() => {
+    let jmpServiceLevelOrder = get(jmpColorScore, colorPath);
+    if (jmpServiceLevelOrder) {
+      jmpServiceLevelOrder = Object.keys(jmpServiceLevelOrder);
+    }
     if (selector === "period") {
       const period = data.map((x) => ({
         name: x.name,
@@ -57,31 +62,59 @@ const ChartVisual = ({ chartConfig, loading }) => {
       return orderBy(results, ["value"], ["asc"]);
     }
     if (isStack) {
-      const transform = data.map((d) => {
+      let temp = data.map((d) => {
         const serviceLevel = get(d, path);
         const stack = Object.keys(serviceLevel).map((key) => {
-          return {
+          const findJMPConfig = get(jmpColorScore, `${colorPath}.${key}`);
+          let obj = {
             name: capitalize(key),
             title: capitalize(key),
             value: serviceLevel[key],
-            total: serviceLevel[key],
+            total: !isNil(findJMPConfig?.score)
+              ? serviceLevel[key] * findJMPConfig.score
+              : serviceLevel[key],
+            color: findJMPConfig.color,
           };
+          if (jmpServiceLevelOrder) {
+            obj = { ...obj, order: jmpServiceLevelOrder.indexOf(key) + 1 };
+          }
+          return obj;
         });
         return {
           name: d.loc,
           title: d.loc,
-          stack: stack,
+          stack: orderBy(stack, "order"),
         };
       });
+      // order by score
+      temp = orderBy(temp, [
+        function (e) {
+          return sumBy(e.stack, "total");
+        },
+      ]);
+      let transform = temp.filter((d) => sumBy(d.stack, "value") > 0);
+      if (showEmpty) {
+        transform = temp;
+      }
       return transform;
     }
-  }, [data, path, selector, colorPath, isStack]);
+  }, [data, path, selector, colorPath, isStack, showEmpty]);
 
   return (
     <Col key={`col-${type}-${index}`} span={span} className="chart-card">
       <Card>
         <Row className="chart-header" justify="space-between" align="middle">
           <h3>{title}</h3>
+          {showSwitcher && isStack && (
+            <Checkbox
+              onChange={() => {
+                setShowEmpty(!showEmpty);
+              }}
+              checked={showEmpty}
+            >
+              Show empty values
+            </Checkbox>
+          )}
           {showSwitcher && (
             <Space align="center">
               <span>Show By County</span>
