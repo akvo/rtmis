6cc66c652fe28b0e255563f8f6111b0509b9a30b
lightme-fan <fanilo.and@onja.org>
2022-09-02 11:54:27 +0300

    [#677] - add custom style for table

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -53,7 +52,18 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+
+def setup(app):
+    app.add_css_file('css/custom.css')
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
 
 # RTD Theme
 html_theme_options = {
