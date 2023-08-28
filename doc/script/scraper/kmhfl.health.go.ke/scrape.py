984c99673febdf3c71f5979ddf14e5c33a9def64
dedenbangkit <mail@dedenbangkit.com>
2023-08-28 09:50:22 +0700

    [#711] Update Community Health Units kmhfl.health.go.ke

--- doc/script/scraper/kmhfl.health.go.ke/scrape.py
+++ doc/script/scraper/kmhfl.health.go.ke/scrape.py
@@ -2,29 +2,41 @@ import requests as r
 import pandas as pd
 import json
 
-URL = "https://api.kmhfl.health.go.ke/api/facilities/material/?fields="
-FIELDS = "id,code,name,regulatory_status_name,facility_type_name,owner_name,county,constituency,ward_name,keph_level,operation_status_name"
-
 HEADERS = {
     "Accept": "application/json, */*",
     "Authorization": "Bearer TMDwn95aV5UuNcBdKPXes0JXRD53xz",
 }
 
-results = []
 
-api_url = f"{URL}?{FIELDS}"
-while True:
-    print("[GET]", api_url)
-    data = r.get(api_url, headers=HEADERS)
-    data = data.json()
-    results += data["results"]
-    if data.get("next"):
-        api_url = data["next"]
-    else:
-        break
+def getit(url, fields, filename="facilities"):
+    results = []
 
-pd.DataFrame(results).to_csv("./facilities.csv", index=False)
-json_data = json.dumps(results, indent=4)
+    api_url = f"{url}?{fields}"
+    while True:
+        print("[GET]", api_url)
+        data = r.get(api_url, headers=HEADERS)
+        data = data.json()
+        results += data["results"]
+        if data.get("next"):
+            api_url = data["next"]
+        else:
+            break
 
-with open("facilities-all.json", "w") as f:
-    f.write(json_data)
+    pd.DataFrame(results).to_csv(f"./{filename}.csv", index=False)
+    json_data = json.dumps(results, indent=4)
+
+    with open(f"{filename}-all.json", "w") as f:
+        f.write(json_data)
+
+
+# Health Facilities
+URL = "https://api.kmhfl.health.go.ke/api/facilities/material/?fields="
+FIELDS = "id,code,name,regulatory_status_name,facility_type_name,owner_name,county,constituency,ward_name,keph_level,operation_status_name"
+print("[COLLECTING] HEALTH FACILITIES")
+getit(URL, FIELDS)
+
+# Community Units
+URL = "https://api.kmhfl.health.go.ke/api/chul/units/?fields="
+FIELDS = "id,code,name,status_name,date_established,facility,facility_name,facility_county,facility_subcounty,facility_ward,facility_constituency"
+print("[COLLECTING] COMMUNITY UNITS")
+getit(URL, FIELDS, filename="community_units")
