390b9463c65710fcf3463c9095bf64eb67c55794
Iwan Firmawan <firmawaneiwan@gmail.com>
2023-11-22 08:20:18 +0700

    [#714] Add new component: Input Attributes

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -28,3 +28,4 @@ export { default as ContactForm } from "./contact-form/ContactForm";
 export { default as ManageDataTab } from "./tabs/MasterDataTab";
 export { default as AdministrationFilters } from "./filters/AdministrationFilters";
 export { default as DetailTable } from "./DetailTable";
+export { default as InputAttributes } from "./InputAttributes";
