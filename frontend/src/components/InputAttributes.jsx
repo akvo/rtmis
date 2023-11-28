2322ffb3d3e57c5c8b0ae4762e31f5f6c05008ba
ifirmawan <firmawaneiwan@gmail.com>
2023-11-28 17:12:32 +0700

    [#723] Fix inputType to handle all types: value, option, multiple & aggregate

--- frontend/src/components/InputAttributes.jsx
+++ frontend/src/components/InputAttributes.jsx
@@ -1,37 +1,90 @@
-import { Row, Col, Form, Input, Select, Spin } from "antd";
+import { Row, Col, Form, Input, Select, Spin, Typography, Space } from "antd";
 
 const { Option } = Select;
+const { Text } = Typography;
 
-const InputType = ({ field, name, options, mode }) => {
-  return options.length ? (
-    <div className="form-row">
-      <Form.Item name={[field.name, name]} label={name}>
-        <Select
-          getPopupContainer={(trigger) => trigger.parentNode}
-          placeholder={`Select ${name}...`}
-          allowClear
-          mode={mode}
-        >
-          {options?.map((opt, index) => (
-            <Option key={index} value={opt}>
-              {opt}
-            </Option>
-          ))}
-        </Select>
-      </Form.Item>
-    </div>
-  ) : (
-    <Row className="form-row">
-      <Col span={24}>
-        <Form.Item name={[field.name, name]} label={name}>
-          <Input type="number" />
-        </Form.Item>
-      </Col>
-    </Row>
+const InputType = ({ field, name, options, type }) => {
+  return (
+    <>
+      {type === "value" && (
+        <Row className="form-row">
+          <Col span={24}>
+            <Form.Item name={[field.name, name]} label={name}>
+              <Input type="number" />
+            </Form.Item>
+          </Col>
+        </Row>
+      )}
+      {type === "option" && (
+        <div className="form-row">
+          <Form.Item name={[field.name, name]} label={name}>
+            <Select
+              getPopupContainer={(trigger) => trigger.parentNode}
+              placeholder={`Select ${name}...`}
+              allowClear
+            >
+              {options?.map((opt, index) => (
+                <Option key={index} value={opt}>
+                  {opt}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </div>
+      )}
+      {type === "multiple_option" && (
+        <div className="form-row">
+          <Form.Item name={[field.name, name]} label={name}>
+            <Select
+              getPopupContainer={(trigger) => trigger.parentNode}
+              placeholder={`Select ${name}...`}
+              mode="multiple"
+              allowClear
+            >
+              {options?.map((opt, index) => (
+                <Option key={index} value={opt}>
+                  {opt}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </div>
+      )}
+      {type === "aggregate" && (
+        <Row className="form-row">
+          <Col span={6}>
+            <Text>{name}</Text>
+          </Col>
+          <Col span={18}>
+            <Form.List name={[field.name, "aggregate"]}>
+              {(items) => (
+                <>
+                  {items.map(({ key: aggKey, name: aggName }) => (
+                    <Space key={aggKey} direction="horizontal">
+                      {options.map((opt, opx) => {
+                        return (
+                          <Form.Item
+                            label={opt}
+                            name={[aggName, opt]}
+                            key={`${aggKey}-${opx}`}
+                          >
+                            <Input type="number" placeholder={opt} />
+                          </Form.Item>
+                        );
+                      })}
+                    </Space>
+                  ))}
+                </>
+              )}
+            </Form.List>
+          </Col>
+        </Row>
+      )}
+    </>
   );
 };
 
-const InputAttributes = ({ attributes = [], loading = false, mode = null }) => {
+const InputAttributes = ({ attributes = [], loading = false }) => {
   return (
     <Spin spinning={loading} tip="Loading...">
       <Form.List name="attributes">
@@ -39,12 +92,7 @@ const InputAttributes = ({ attributes = [], loading = false, mode = null }) => {
           return (
             <>
               {fields.map((field, index) => (
-                <InputType
-                  field={field}
-                  key={index}
-                  mode={mode}
-                  {...attributes?.[index]}
-                />
+                <InputType field={field} key={index} {...attributes?.[index]} />
               ))}
             </>
           );
