762e0512dfaf0254520b7dae450dabf94656575b
dedenbangkit <mail@dedenbangkit.com>
2023-12-01 17:09:55 +0700

    [#729] Decode assignment passcode to UI

--- backend/api/v1/v1_mobile/serializers.py
+++ backend/api/v1/v1_mobile/serializers.py
@@ -17,17 +17,17 @@ class MobileFormSerializer(serializers.ModelSerializer):
 
     @extend_schema_field(OpenApiTypes.URI)
     def get_url(self, obj):
-        return f'/form/{obj.id}'
+        return f"/form/{obj.id}"
 
     class Meta:
         model = Forms
-        fields = ['id', 'version', 'url']
+        fields = ["id", "version", "url"]
 
 
 class MobileAssignmentFormsSerializer(serializers.Serializer):
     code = CustomCharField(max_length=255, write_only=True)
     name = serializers.CharField(read_only=True)
-    syncToken = serializers.CharField(source='token', read_only=True)
+    syncToken = serializers.CharField(source="token", read_only=True)
     formsUrl = serializers.SerializerMethodField()
 
     @extend_schema_field(MobileFormSerializer(many=True))
@@ -37,11 +37,11 @@ class MobileAssignmentFormsSerializer(serializers.Serializer):
     def validate_code(self, value):
         passcode = CustomPasscode().encode(value)
         if not MobileAssignment.objects.filter(passcode=passcode).exists():
-            raise serializers.ValidationError('Invalid passcode')
+            raise serializers.ValidationError("Invalid passcode")
         return value
 
     class Meta:
-        fields = ['name', 'syncToken', 'formsUrl', 'code']
+        fields = ["name", "syncToken", "formsUrl", "code"]
 
 
 class IdAndNameRelatedField(serializers.PrimaryKeyRelatedField):
@@ -50,35 +50,33 @@ class IdAndNameRelatedField(serializers.PrimaryKeyRelatedField):
 
     def to_representation(self, value):
         return {
-            'id': value.pk,
-            'name': value.name,
+            "id": value.pk,
+            "name": value.name,
         }
 
 
 class MobileAssignmentSerializer(serializers.ModelSerializer):
     forms = IdAndNameRelatedField(queryset=Forms.objects.all(), many=True)
     administrations = IdAndNameRelatedField(
-            queryset=Administration.objects.all(), many=True)
+        queryset=Administration.objects.all(), many=True
+    )
+    passcode = serializers.SerializerMethodField()
 
     class Meta:
         model = MobileAssignment
-        fields = [
-            'id',
-            'name',
-            'passcode',
-            'forms',
-            'administrations'
-        ]
-        read_only_fields = ['passcode']
+        fields = ["id", "name", "passcode", "forms", "administrations"]
+        read_only_fields = ["passcode"]
 
     def create(self, validated_data: Dict[str, Any]):
-        user = self.context.get('request').user
+        user = self.context.get("request").user
         token = RefreshToken.for_user(user)
         passcode = CustomPasscode().encode(generate_random_string(8))
-        validated_data.update({
-            'user': user, 'token': token, 'passcode': passcode})
+        validated_data.update({"user": user, "token": token, "passcode": passcode})
         instance = super().create(validated_data)
         return instance
+
+    def get_passcode(self, obj):
+        return CustomPasscode().decode(obj.passcode)
 
 
 class MobileApkSerializer(serializers.Serializer):
@@ -91,4 +89,4 @@ class MobileApkSerializer(serializers.Serializer):
 
     class Meta:
         model = MobileApk
-        fields = ['apk_version', 'apk_url', 'created_at']
+        fields = ["apk_version", "apk_url", "created_at"]
