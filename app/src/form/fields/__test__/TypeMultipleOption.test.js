7df840cf8692b06bc553250f0db90355464c7c95
dedenbangkit <mail@dedenbangkit.com>
2023-12-04 15:40:25 +0700

    [#740] Disable radio, show search when option > 2

--- app/src/form/fields/__test__/TypeMultipleOption.test.js
+++ app/src/form/fields/__test__/TypeMultipleOption.test.js
@@ -4,32 +4,6 @@ import TypeMultipleOption from '../TypeMultipleOption';
 import { FormState } from '../../../store';
 
 describe('TypeMultipleOption', () => {
-  test('renders checkbox options correctly when option length is less than or equal to 3', () => {
-    const option = [
-      { name: 'option1', label: 'Option 1' },
-      { name: 'option2', label: 'Option 2' },
-      { name: 'option3', label: 'Option 3' },
-    ];
-    const onChange = jest.fn();
-
-    const { getByText } = render(
-      <TypeMultipleOption
-        onChange={onChange}
-        values={{}}
-        id="multipleOptionId"
-        name="Multiple Option"
-        option={option}
-      />,
-    );
-
-    expect(getByText('Option 1')).toBeDefined();
-    expect(getByText('Option 2')).toBeDefined();
-    expect(getByText('Option 3')).toBeDefined();
-
-    fireEvent.press(getByText('Option 1'));
-    expect(onChange).toHaveBeenCalledWith('multipleOptionId.0', 'option1');
-  });
-
   test('renders MultiSelect correctly when option length is greater than 3', () => {
     const option = [
       { name: 'option1', label: 'Option 1' },
