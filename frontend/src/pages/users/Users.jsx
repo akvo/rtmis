8e547b1c04c8db209d6f088e31cc6d6c056dfcb1
navins94 <nikshingote@gmail.com>
2023-12-07 15:26:55 +0530

    [#761] Update table style

--- frontend/src/pages/users/Users.jsx
+++ frontend/src/pages/users/Users.jsx
@@ -229,57 +229,61 @@ const Users = () => {
           </Link>
         }
       />
-      <UserFilters
-        query={query}
-        setQuery={setQuery}
-        fetchData={fetchData}
-        pending={pending}
-        setPending={setPending}
-        loading={loading}
-      />
-      <Divider />
-      <Card
-        style={{ padding: 0, minHeight: "40vh" }}
-        bodyStyle={{ padding: 0 }}
-      >
-        <Table
-          columns={columns}
-          rowClassName={() => "editable-row"}
-          dataSource={dataset}
-          loading={loading}
-          onChange={handleChange}
-          pagination={{
-            showSizeChanger: false,
-            current: currentPage,
-            total: totalCount,
-            pageSize: 10,
-            showTotal: (total, range) =>
-              `Results: ${range[0]} - ${range[1]} of ${total} users`,
-          }}
-          rowKey="id"
-          expandable={{
-            expandedRowRender: (record) => (
-              <UserDetail
-                record={record}
-                setDeleteUser={setDeleteUser}
-                deleting={deleting}
-              />
-            ),
-            expandIcon: ({ expanded, onExpand, record }) =>
-              expanded ? (
-                <CloseSquareOutlined
-                  onClick={(e) => onExpand(record, e)}
-                  style={{ color: "#e94b4c" }}
-                />
-              ) : (
-                <PlusSquareOutlined
-                  onClick={(e) => onExpand(record, e)}
-                  style={{ color: "#7d7d7d" }}
-                />
-              ),
-          }}
-        />
-      </Card>
+      <div className="table-section">
+        <div className="table-wrapper">
+          <UserFilters
+            query={query}
+            setQuery={setQuery}
+            fetchData={fetchData}
+            pending={pending}
+            setPending={setPending}
+            loading={loading}
+          />
+          <Divider />
+          <div
+            style={{ padding: 0, minHeight: "40vh" }}
+            bodyStyle={{ padding: 0 }}
+          >
+            <Table
+              columns={columns}
+              rowClassName={() => "editable-row"}
+              dataSource={dataset}
+              loading={loading}
+              onChange={handleChange}
+              pagination={{
+                showSizeChanger: false,
+                current: currentPage,
+                total: totalCount,
+                pageSize: 10,
+                showTotal: (total, range) =>
+                  `Results: ${range[0]} - ${range[1]} of ${total} users`,
+              }}
+              rowKey="id"
+              expandable={{
+                expandedRowRender: (record) => (
+                  <UserDetail
+                    record={record}
+                    setDeleteUser={setDeleteUser}
+                    deleting={deleting}
+                  />
+                ),
+                expandIcon: ({ expanded, onExpand, record }) =>
+                  expanded ? (
+                    <CloseSquareOutlined
+                      onClick={(e) => onExpand(record, e)}
+                      style={{ color: "#e94b4c" }}
+                    />
+                  ) : (
+                    <PlusSquareOutlined
+                      onClick={(e) => onExpand(record, e)}
+                      style={{ color: "#7d7d7d" }}
+                    />
+                  ),
+              }}
+            />
+          </div>
+        </div>
+      </div>
       <Modal
         visible={deleteUser}
         onCancel={() => setDeleteUser(null)}
