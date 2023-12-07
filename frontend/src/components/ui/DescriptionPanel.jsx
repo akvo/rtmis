9e5eeeb5319aa845a7d23414a7952315c33d90cb
navins94 <nikshingote@gmail.com>
2023-12-07 11:57:32 +0530

    [#759] Update title of remaining pages

--- frontend/src/components/ui/DescriptionPanel.jsx
+++ frontend/src/components/ui/DescriptionPanel.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
 function DescriptionPanel({ description, title }) {
-  if (!description) {
-    return "";
-  }
   return (
     <div className="description-panel">
       {title && <h2>{title}</h2>}
-      <div className="description-paragraph">{description}</div>
+      {description && (
+        <div className="description-paragraph">{description}</div>
+      )}
     </div>
   );
 }
