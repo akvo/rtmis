02034fac5ec67747d0a578b488f85426a8f485f0
zuhdil <zuhdil@gmail.com>
2023-12-22 16:51:16 +0700

    [#816] Implement SQL generator for Entities

--- backend/api/v1/v1_mobile/management/commands/generate_sqlite.py
+++ backend/api/v1/v1_mobile/management/commands/generate_sqlite.py
@@ -1,14 +1,25 @@
 from django.core.management import BaseCommand
 from utils.custom_generator import generate_sqlite
-from api.v1.v1_profile.models import Administration
+from api.v1.v1_profile.models import Administration, Entity, EntityData
 from api.v1.v1_users.models import Organisation
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        conn, file = generate_sqlite(Administration)
-        print(f'{file} Generated Successfully')
-        conn.close()
-        conn, file = generate_sqlite(Organisation)
-        print(f'{file} Generated Successfully')
-        conn.close()
+        file = generate_sqlite(Administration)
+        self.log_generated(file, Administration)
+        file = generate_sqlite(Organisation)
+        self.log_generated(file, Organisation)
+        file = generate_sqlite(Entity)
+        self.log_generated(file, Entity)
+        file = generate_sqlite(EntityData)
+        self.log_generated(file, EntityData)
+
+    def log_generated(self, file, model):
+        message = f'{file} Generated Successfully' \
+            if file \
+            else (
+                f'Failed to generate {model._meta.db_table}, '
+                'possibly empty data'
+            )
+        print(message)
