17a2182c3d8589227c26b3a1bb25b15c85b16815
dedenbangkit <mail@dedenbangkit.com>
2023-12-26 14:29:10 +0700

    [#819] Autofield type of question handler

--- backend/api/v1/v1_data/serializers.py
+++ backend/api/v1/v1_data/serializers.py
@@ -162,6 +162,7 @@ class SubmitFormSerializer(serializers.Serializer):
                     QuestionTypes.text,
                     QuestionTypes.photo,
                     QuestionTypes.date,
+                    QuestionTypes.autofield
             ]:
                 name = answer.get("value")
             elif answer.get("question").type == QuestionTypes.cascade:
