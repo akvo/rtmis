0fe0a5cc9bec1d57bb7076d33898a694c6684520
navins94 <nikshingote@gmail.com>
2024-01-04 11:32:56 +0530

    [#837] Add flatlist

--- app/src/form/FormContainer.js
+++ app/src/form/FormContainer.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useMemo, useEffect } from 'react';
 import { BaseLayout } from '../components';
-import { ScrollView, View } from 'react-native';
+import { ScrollView, View, FlatList } from 'react-native';
 import { Formik } from 'formik';
 import { styles } from './styles';
 import { FormNavigation, QuestionGroupList } from './support';
@@ -75,10 +75,14 @@ const FormContainer = ({ forms, initialValues = {}, onSubmit, onSave, setShowDia
     }
   };
 
+  const style = {
+    flex: 1,
+  };
+
   return (
     <>
-      <ScrollView>
-        <BaseLayout.Content>
+      <BaseLayout.Content>
+        <View style={style}>
           {!showQuestionGroupList ? (
             <Formik
               innerRef={formRef}
@@ -88,8 +92,11 @@ const FormContainer = ({ forms, initialValues = {}, onSubmit, onSave, setShowDia
               validateOnChange={true}
             >
               {({ setFieldValue, values }) => (
-                <View style={styles.formContainer}>
-                  {formDefinition?.question_group?.map((group) => {
+                <FlatList
+                  scrollEnabled={true}
+                  data={formDefinition?.question_group}
+                  keyExtractor={(item) => `group-${item.id}`}
+                  renderItem={({ item: group }) => {
                     if (activeGroup !== group.id) {
                       return '';
                     }
@@ -102,8 +109,9 @@ const FormContainer = ({ forms, initialValues = {}, onSubmit, onSave, setShowDia
                         values={values}
                       />
                     );
-                  })}
-                </View>
+                  }}
+                  extraData={activeGroup}
+                />
               )}
             </Formik>
           ) : (
@@ -115,8 +123,8 @@ const FormContainer = ({ forms, initialValues = {}, onSubmit, onSave, setShowDia
               setShowQuestionGroupList={setShowQuestionGroupList}
             />
           )}
-        </BaseLayout.Content>
-      </ScrollView>
+        </View>
+      </BaseLayout.Content>
       <View>
         <FormNavigation
           currentGroup={currentGroup}
