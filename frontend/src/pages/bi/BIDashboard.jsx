54982c9b07ac732ef19778ee5bf1588dd5e97e69
navins94 <nikshingote@gmail.com>
2024-01-06 15:44:18 +0530

    [#854] Add power bi dashboard

---
+++ frontend/src/pages/bi/BIDashboard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Affix } from "antd";
+import ResponsiveEmbed from "react-responsive-embed";
+import { useParams } from "react-router-dom";
+import "./style.scss";
+
+const BIDashboard = () => {
+  const { formId } = useParams();
+  const powerBIDashboard = window?.powerBIDashboard;
+  const current = powerBIDashboard?.find((x) => x.form_id === parseInt(formId));
+
+  return (
+    <div id="powerbi-dashboard">
+      {current?.content.map((c, ci) => {
+        const componentKey = `${c.key}-${ci}`;
+        switch (c.key) {
+          case "embed":
+            return (
+              <div
+                key={componentKey}
+                className="main-wrapper"
+                style={c?.style ? c.style : {}}
+              >
+                <ResponsiveEmbed src={c.link} />
+                <div className="blank-white" />
+              </div>
+            );
+          default:
+            return (
+              <Affix key={componentKey} className="sticky-wrapper">
+                <div className="page-title-wrapper">
+                  <h1 style={c?.style ? c.style : {}}>{c.text}</h1>
+                </div>
+              </Affix>
+            );
+        }
+      })}
+    </div>
+  );
+};
+
+export default React.memo(BIDashboard);
