2c7ce1bb170d7fffafb199939c65ea5cee4c114a
ifirmawan <firmawaneiwan@gmail.com>
2024-01-08 21:36:36 +0700

    [#832] Create separate func to handle prefilled administration template

--- backend/utils/administration_upload_template.py
+++ backend/utils/administration_upload_template.py
@@ -1,4 +1,5 @@
 import os
+from io import BytesIO
 from typing import List
 from django.db.models import QuerySet
 from django.utils import timezone
@@ -16,8 +17,6 @@ from api.v1.v1_users.models import SystemUser
 def generate_excel(
     user: SystemUser,
     attributes: List[int] = [],
-    level: int = None,
-    prefilled: bool = False,
 ):
     directory = 'tmp/administrations-template'
     os.makedirs(directory, exist_ok=True)
@@ -27,15 +26,13 @@ def generate_excel(
     filepath = f"./{directory}/{filename}"
     if os.path.exists(filepath):
         os.remove(filepath)
-    generate_template(filepath, attributes, level, prefilled)
+    generate_template(filepath=filepath, attributes=attributes)
     return filepath
 
 
 def generate_template(
-        filepath,
-        attributes: List[int] = [],
-        level: int = None,
-        prefilled: bool = False
+    filepath,
+    attributes: List[int] = [],
 ):
     level_headers = [
             f'{lvl.id}|{lvl.name}' for lvl
@@ -62,30 +59,64 @@ def generate_template(
     })
     for col_num, value in enumerate(data.columns.values):
         worksheet.write(0, col_num, value, header_format)
-    # get administrations list when level is exists
-    if level and prefilled:
-        administrations = Administration.objects\
-            .filter(level_id__lte=level).all()
-        for adx, adm in enumerate(administrations):
-            find_col = next((
-                lx for lx, lvl in enumerate(level_headers)
-                if str(adm.level_id) in lvl
-            ), -1)
-            if find_col >= 0:
-                worksheet.write(adx + 1, find_col, adm.name)
-            if adm.path:
-                adm_parents = [
-                    list(
-                        filter(lambda item: item.id == int(p), administrations)
-                    )
-                    for p in list(filter(
-                        lambda path: path, adm.path.split(".")
-                    ))
-                ]
-                for parent_col, pl in enumerate(adm_parents):
-                    [parent] = pl
-                    worksheet.write(adx + 1, parent_col, parent.name)
     writer.save()
+
+
+def generate_prefilled_template(
+    attributes: List[int] = [],
+    level: int = None
+):
+    excel_file = BytesIO()
+    level_headers = [
+            f'{lvl.id}|{lvl.name}' for lvl
+            in Levels.objects.order_by('level').all()]
+    attribute_headers = generate_attribute_headers(
+        AdministrationAttribute.objects.filter(
+            id__in=attributes).order_by('id'))
+    columns = level_headers + attribute_headers
+    data = pd.DataFrame(columns=columns, index=[0])
+    writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
+    data.to_excel(
+            writer,
+            sheet_name='data',
+            startrow=1,
+            header=False,
+            index=False)
+    workbook = writer.book
+    worksheet = writer.sheets['data']
+    header_format = workbook.add_format({
+        'bold': True,
+        'text_wrap': True,
+        'valign': 'top',
+        'border': 1
+    })
+    for col_num, value in enumerate(data.columns.values):
+        worksheet.write(0, col_num, value, header_format)
+    administrations = Administration.objects
+    if level:
+        administrations = administrations.filter(level_id__lte=level)
+    administrations = administrations.all()
+    for adx, adm in enumerate(administrations):
+        find_col = next((
+            lx for lx, lvl in enumerate(level_headers)
+            if str(adm.level_id) in lvl
+        ), -1)
+        if find_col >= 0:
+            worksheet.write(adx + 1, find_col, adm.name)
+        if adm.path:
+            adm_parents = [
+                list(
+                    filter(lambda item: item.id == int(p), administrations)
+                )
+                for p in list(filter(
+                    lambda path: path, adm.path.split(".")
+                ))
+            ]
+            for parent_col, pl in enumerate(adm_parents):
+                [parent] = pl
+                worksheet.write(adx + 1, parent_col, parent.name)
+    writer.save()
+    return excel_file.getvalue()
 
 
 def generate_attribute_headers(
