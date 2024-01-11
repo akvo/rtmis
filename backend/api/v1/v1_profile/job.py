37c251a07caf38e7bf3399c2229392cced75c9d5
ifirmawan <firmawaneiwan@gmail.com>
2024-01-11 14:24:46 +0700

    [#832] Remove unused send attachment process

--- backend/api/v1/v1_profile/job.py
+++ backend/api/v1/v1_profile/job.py
@@ -1,6 +1,5 @@
 import logging
 from api.v1.v1_jobs.models import Jobs, JobStatus
-from utils.email_helper import send_email, EmailTypes
 from utils.administration_upload_template import generate_prefilled_template
 from django.utils import timezone
 
@@ -11,32 +10,11 @@ def download_prefilled_administrations(job_id: int):
     try:
         job = Jobs.objects.get(pk=job_id)
         job_info = job.info
-        excel_file = generate_prefilled_template(
+        return generate_prefilled_template(
+            job_result=job.result,
             attributes=job_info['attributes'],
             level=job_info['level'],
         )
-        data = {
-            'send_to': [job.user.email],
-            'listing': [{
-                'name': "Download Date",
-                'value': job.created.strftime("%m-%d-%Y, %H:%M:%S"),
-            }],
-        }
-        filename = (
-            f"{timezone.now().strftime('%Y%m%d%H%M%S')}-{job.user.pk}-"
-            "administrations-template.xlsx"
-        )
-        excel = {
-            'name': filename,
-            'file': excel_file
-        }
-        send_email(
-            context=data,
-            excel=excel,
-            type=EmailTypes.administration_prefilled,
-            content_type='application/vnd.ms-excel'
-        )
-        return True
     except Exception as unknown_error:
         print("unknown_error", unknown_error)
         logger.error({
