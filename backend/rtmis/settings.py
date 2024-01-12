e64223ffd1b3b130e82ed210983f17f99531f302
dedenbangkit <mail@dedenbangkit.com>
2024-01-12 07:43:41 +0700

    Modify qcluster tiemout and retry

--- backend/rtmis/settings.py
+++ backend/rtmis/settings.py
@@ -215,8 +215,8 @@ EMAIL_FROM = environ.get("EMAIL_FROM") or 'noreply@akvo.org'
 Q_CLUSTER = {
     'name': 'DjangORM',
     'workers': 4,
-    'timeout': 90,
-    'retry': 120,
+    'timeout': 600,
+    'retry': 3,
     'queue_limit': 50,
     'bulk': 10,
     'orm': 'default'
