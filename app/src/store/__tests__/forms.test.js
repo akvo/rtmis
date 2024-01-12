e57539da86b83af8f10a4815ec8456c68d472fdc
ifirmawan <firmawaneiwan@gmail.com>
2024-01-12 13:41:04 +0700

    [#904] Remove completely questionGroupListCurrentValues state

--- app/src/store/__tests__/forms.test.js
+++ app/src/store/__tests__/forms.test.js
@@ -4,10 +4,9 @@ import FormState from '../forms';
 describe('FormState', () => {
   it('should initialize with the correct default state', () => {
     const { result } = renderHook(() => FormState.useState());
-    const { form, currentValues, questionGroupListCurrentValues } = result.current;
+    const { form, currentValues } = result.current;
     expect(form).toEqual({});
     expect(currentValues).toEqual({});
-    expect(questionGroupListCurrentValues).toEqual({});
   });
 
   it('should updating the state correctly', () => {
@@ -19,32 +18,14 @@ describe('FormState', () => {
       version: '1.2.0',
       totalGroup: 2,
     };
-    const qg = [
-      {
-        id: 1,
-        name: 'Profile',
-      },
-      {
-        id: 2,
-        name: 'Complain',
-      },
-    ];
-    const qs = [
-      {
-        id: 3,
-        text: 'Description',
-      },
-    ];
     act(() => {
       FormState.update((s) => {
         s.form = selectedForm;
         s.currentValues = { 1: 'John Doe', 2: 12 };
-        s.questionGroupListCurrentValues = { 1: 'John Doe' };
       });
     });
-    const { form, currentValues, questionGroupListCurrentValues } = result.current;
+    const { form, currentValues } = result.current;
     expect(form).toBe(selectedForm);
     expect(currentValues).toEqual({ 1: 'John Doe', 2: 12 });
-    expect(questionGroupListCurrentValues).toEqual({ 1: 'John Doe' });
   });
 });
