cdb6cb0ca23514d817acc1c9eece4903952c5748
dedenbangkit <mail@dedenbangkit.com>
2024-01-15 18:09:22 +0700

    [#931] Remove dbadm from config.js

--- backend/api/v1/v1_data/management/commands/generate_config.py
+++ backend/api/v1/v1_data/management/commands/generate_config.py
@@ -15,41 +15,41 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # fetch all administrations
         print("GENERATING CONFIG JS")
-        all_administrations = {}
-        adm = []
-        administrations = Administration.objects.all()
-        for a in administrations:
-            all_administrations.update({a.name: {"id": a.id, "path": a.path}})
-            adm.append(
-                {
-                    "id": a.id,
-                    "parent": a.parent_id,
-                    "name": a.name,
-                    "path": a.path,
-                    "full_name": a.administration_column,
-                    "level": a.level.level,
-                }
-            )
+        # all_administrations = {}
+        # adm = []
+        # administrations = Administration.objects.all()
+        # for a in administrations:
+        #     all_administrations.update({a.name: {"id": a.id, "path": a.path}})
+        #     adm.append(
+        #         {
+        #             "id": a.id,
+        #             "parent": a.parent_id,
+        #             "name": a.name,
+        #             "path": a.path,
+        #             "full_name": a.administration_column,
+        #             "level": a.level.level,
+        #         }
+        #     )
         # add administration id to topojson
         topojson = open("source/kenya.topojson").read()
-        topojson = json.loads(topojson)
-        geometris_with_id = []
-        for obj in topojson["objects"]["kenya"]["geometries"]:
-            key = obj["properties"]["NAME_3"]
-            find_id = 0
-            if key in all_administrations:
-                find_id = all_administrations[key].get("id")
-            obj["properties"].update({"SHAPE_ADMIN_ID": find_id})
-            geometris_with_id.append(obj)
-        topojson["objects"]["kenya"]["geometries"] = geometris_with_id
-        # write new topojson file
-        new_topojson_file = "source/kenya_with_admin_id.topojson"
-        with open(new_topojson_file, "w") as outfile:
-            json.dump(topojson, outfile)
-        # write administration_file
-        administration_json = "source/administration.json"
-        with open(administration_json, "w") as outfile:
-            json.dump(adm, outfile)
+        # topojson = json.loads(topojson)
+        # geometris_with_id = []
+        # for obj in topojson["objects"]["kenya"]["geometries"]:
+        #     key = obj["properties"]["NAME_3"]
+        #     find_id = 0
+        #     if key in all_administrations:
+        #         find_id = all_administrations[key].get("id")
+        #     obj["properties"].update({"SHAPE_ADMIN_ID": find_id})
+        #     geometris_with_id.append(obj)
+        # topojson["objects"]["kenya"]["geometries"] = geometris_with_id
+        # # write new topojson file
+        # new_topojson_file = "source/kenya_with_admin_id.topojson"
+        # with open(new_topojson_file, "w") as outfile:
+        #     json.dump(topojson, outfile)
+        # # write administration_file
+        # administration_json = "source/administration.json"
+        # with open(administration_json, "w") as outfile:
+        #     json.dump(adm, outfile)
         # write visualisation_json
         # visualisation_json = "source/config/visualisation.json"
         highlights_json = "source/config/highlights.json"
@@ -97,11 +97,12 @@ class Command(BaseCommand):
                     ";",
                     # "var visualisation=",
                     # open(visualisation_json).read(), ";",
-                    "var dbadm=",
-                    open(administration_json).read(),
-                    ";",
+                    # "var dbadm=",
+                    # open(administration_json).read(),
+                    # ";",
                     "var topojson=",
-                    open(new_topojson_file).read(),
+                    # open(new_topojson_file).read(),
+                    topojson,
                     ";",
                     "var levels=",
                     json.dumps(levels),
@@ -114,10 +115,10 @@ class Command(BaseCommand):
             )
         )
         open("source/config/config.min.js", "w").write(min_config)
-        os.remove(administration_json)
+        # os.remove(administration_json)
         del levels
         del forms
         del min_config
-        del all_administrations
-        del adm
+        # del all_administrations
+        # del adm
         async_task("api.v1.v1_data.functions.refresh_materialized_data")
