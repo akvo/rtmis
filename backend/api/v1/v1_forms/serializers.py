f50f5ae29202d5066f88253017dd333c811cf93c
dedenbangkit <mail@dedenbangkit.com>
2024-01-16 04:58:09 +0700

    Question group order by order

--- backend/api/v1/v1_forms/serializers.py
+++ backend/api/v1/v1_forms/serializers.py
@@ -342,8 +342,9 @@ class FormDataSerializer(serializers.ModelSerializer):
     @extend_schema_field(FormDataQuestionGroupSerializer(many=True))
     def get_question_group(self, instance: Forms):
         return FormDataQuestionGroupSerializer(
-            instance=instance.form_question_group.all(), many=True
-        ).order_by('order').data
+            instance=instance.form_question_group.all().order_by('order'),
+            many=True
+        ).data
 
     class Meta:
         model = Forms
