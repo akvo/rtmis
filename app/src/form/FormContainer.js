c3e67876fa21f0dc5e6c31680246598848433cdc
ifirmawan <firmawaneiwan@gmail.com>
2024-01-16 12:17:21 +0700

    [#904] Remove unused codes & setFieldValue due duplication setState

--- app/src/form/FormContainer.js
+++ app/src/form/FormContainer.js
@@ -5,8 +5,6 @@ import { FormNavigation, QuestionGroupList } from './support';
 import QuestionGroup from './components/QuestionGroup';
 import { transformForm, generateDataPointName } from './lib';
 import { FormState } from '../store';
-import { Dialog } from '@rneui/themed';
-import { i18n } from '../lib';
 
 // TODO:: Allow other not supported yet
 // TODO:: Repeat group not supported yet
@@ -42,8 +40,6 @@ const FormContainer = ({ forms, onSubmit, setShowDialogMenu }) => {
   const currentValues = FormState.useState((s) => s.currentValues);
   const cascades = FormState.useState((s) => s.cascades);
   const activeLang = FormState.useState((s) => s.lang);
-  const trans = i18n.text(activeLang);
-  const formLoading = FormState.useState((s) => s.loading);
 
   const formDefinition = transformForm(forms, activeLang);
 
@@ -51,19 +47,12 @@ const FormContainer = ({ forms, onSubmit, setShowDialogMenu }) => {
     return formDefinition.question_group.find((qg) => qg.id === activeGroup);
   }, [formDefinition, activeGroup]);
 
-  const handleOnSubmitForm = (values) => {
-    const results = checkValuesBeforeCallback(values);
+  const handleOnSubmitForm = () => {
+    const results = checkValuesBeforeCallback(currentValues);
     if (onSubmit) {
       const { dpName, dpGeo } = generateDataPointName(forms, currentValues, cascades);
       onSubmit({ name: dpName, geo: dpGeo, answers: results });
     }
-  };
-
-  const setFieldValue = (id, value) => {
-    const _values = { ...currentValues, [id]: value };
-    FormState.update((s) => {
-      s.currentValues = _values;
-    });
   };
 
   return (
@@ -71,7 +60,7 @@ const FormContainer = ({ forms, onSubmit, setShowDialogMenu }) => {
       <BaseLayout.Content>
         <View style={style}>
           {!showQuestionGroupList ? (
-            <QuestionGroup index={activeGroup} group={currentGroup} setFieldValue={setFieldValue} />
+            <QuestionGroup index={activeGroup} group={currentGroup} />
           ) : (
             <QuestionGroupList
               form={formDefinition}
@@ -85,7 +74,6 @@ const FormContainer = ({ forms, onSubmit, setShowDialogMenu }) => {
       <View>
         <FormNavigation
           currentGroup={currentGroup}
-          // formRef={formRef}
           onSubmit={handleOnSubmitForm}
           activeGroup={activeGroup}
           setActiveGroup={setActiveGroup}
