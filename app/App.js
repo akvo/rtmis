029cd2ae293cbd15636e0642d94c2952119d94fa
ifirmawan <firmawaneiwan@gmail.com>
2024-01-19 20:21:49 +0700

    [#985] Remove unused code in App.js

--- app/App.js
+++ app/App.js
@@ -4,7 +4,6 @@ import NetInfo from '@react-native-community/netinfo';
 import * as Notifications from 'expo-notifications';
 import * as TaskManager from 'expo-task-manager';
 import * as BackgroundFetch from 'expo-background-fetch';
-import BackgroundService from 'react-native-background-actions';
 
 import Navigation from './src/navigation';
 import { conn, query, tables } from './src/database';
@@ -41,27 +40,6 @@ TaskManager.defineTask(SYNC_FORM_SUBMISSION_TASK_NAME, async () => {
     console.error(`[${SYNC_FORM_SUBMISSION_TASK_NAME}] Define task manager failed`, err);
     return BackgroundFetch.Result.Failed;
   }
-});
-
-const sleep = (time) => new Promise((resolve) => setTimeout(() => resolve(), time));
-
-// You can do anything in your task such as network requests, timers and so on,
-// as long as it doesn't touch UI. Once your task completes (i.e. the promise is resolved),
-// React Native will go into "paused" mode (unless there are other tasks running,
-// or there is a foreground app).
-const veryIntensiveTask = async (taskDataArguments) => {
-  // Example of an infinite loop task
-  const { delay } = taskDataArguments;
-  await new Promise(async (resolve) => {
-    for (let i = 0; BackgroundService.isRunning(); i++) {
-      console.log(i);
-      await sleep(delay);
-    }
-  });
-};
-
-BackgroundService.on('expiration', (props) => {
-  console.log('expiration', props);
 });
 
 const App = () => {
@@ -154,25 +132,6 @@ const App = () => {
           await backgroundTask.registerBackgroundTask(a.taskName);
         }
       });
-
-      // const options = {
-      //   taskName: SYNC_FORM_SUBMISSION_TASK_NAME,
-      //   taskTitle: SYNC_FORM_SUBMISSION_TASK_NAME,
-      //   taskDesc: 'Sync data submission',
-      //   taskIcon: {
-      //     name: 'ic_launcher',
-      //     type: 'mipmap',
-      //   },
-      //   parameters: {
-      //     delay: 3000,
-      //   },
-      // };
-
-      // await BackgroundService.start(veryIntensiveTask, options);
-      // await BackgroundService.start(backgroundTask.syncFormSubmission, options);
-      // await BackgroundService.updateNotification({ taskDesc: 'Sync data submission' }); // Only Android, iOS will ignore this call
-      // // iOS will also run everything here in the background until .stop() is called
-      // await BackgroundService.stop();
     } catch (error) {
       console.error('TASK REGISTER ERROR', error);
     }
