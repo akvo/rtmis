ef0483b13181a66045566a1b3a654045177f2096
navins94 <nikshingote@gmail.com>
2024-01-19 19:42:37 +0530

    [#981]

--- app/src/form/support/OptionItem.js
+++ app/src/form/support/OptionItem.js
@@ -5,8 +5,21 @@ import { Text } from 'react-native';
 const OptionItem = ({ label, color, name }, active) => {
   return (
     <View style={[{ padding: 3 }]}>
-      <View style={[{ backgroundColor: active ? '#0047AB' : color || '#FFF', padding: 15 }]}>
-        <Text style={{ fontWeight: color ? 'bold' : 'normal', color: color ? 'white' : 'black' }}>
+      <View
+        style={[
+          {
+            padding: 8,
+            backgroundColor: color ? color : active ? '#bcbcbc' : '#FFF',
+            borderRadius: color ? 5 : 0,
+          },
+        ]}
+      >
+        <Text
+          style={{
+            fontWeight: color ? 'bold' : 'normal',
+            color: color || active ? 'white' : 'black',
+          }}
+        >
           {label || name}
         </Text>
       </View>
