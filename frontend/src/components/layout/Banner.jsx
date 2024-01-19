bb240ea11eafede5a33612a775239d449332d396
navins94 <nikshingote@gmail.com>
2024-01-19 10:19:54 +0530

    [#1000] Update dashboard link

--- frontend/src/components/layout/Banner.jsx
+++ frontend/src/components/layout/Banner.jsx
@@ -96,12 +96,12 @@ const Banner = () => {
     };
     const DashboardMenu = (
       <Menu>
-        {window?.dashboard?.map((d) => (
+        {window?.powerBIDashboard?.map((d) => (
           <Menu.Item
             key={`${d.name}`}
             style={{ fontSize: 16, fontStyle: "italic", padding: 10 }}
           >
-            <Link to={`/${d.page}/${d.form_id}`}>{d.name}</Link>
+            <Link to={`/${d.page}/${d.path}`}>{d.name}</Link>
           </Menu.Item>
         ))}
       </Menu>
