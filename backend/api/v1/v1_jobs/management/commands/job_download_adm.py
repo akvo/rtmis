a0df0b772aeeae1625d33004ceed9f27d375804b
ifirmawan <firmawaneiwan@gmail.com>
2024-01-29 16:30:07 +0700

    [#832] Replace level w/ administration args in job

--- backend/api/v1/v1_jobs/management/commands/job_download_adm.py
+++ backend/api/v1/v1_jobs/management/commands/job_download_adm.py
@@ -4,14 +4,14 @@ from django.core.management import BaseCommand
 from django.utils import timezone
 from django_q.tasks import async_task
 
-from api.v1.v1_profile.models import Levels
+from api.v1.v1_profile.models import Administration
 from api.v1.v1_jobs.constants import JobTypes, JobStatus
 from api.v1.v1_jobs.models import Jobs
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("level", nargs="+", type=int)
+        parser.add_argument("administration", nargs="+", type=int)
         parser.add_argument("user", nargs="+", type=int)
         parser.add_argument(
             "-a",
@@ -24,20 +24,22 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         attributes = options.get("attributes")
-        level_id = options.get("level")[0] if options.get("level") else None
-        last_level = Levels.objects.order_by("-id")[0]
-        level = (
-            Levels.objects.get(pk=options.get("level")[0])
-            if level_id else last_level
-        )
-        level_name = level.name.replace(" ", "_").lower()
+        adm_id = options.get("administration")[0]\
+            if options.get("administration") else None
+        find_adm = Administration.objects.get(pk=adm_id)
+        file_name = find_adm.name.replace(" ", "_").lower() \
+            if find_adm else adm_id
         today = timezone.datetime.today().strftime("%y%m%d")
         out_file = "download-{0}-{1}-{2}.xlsx".format(
-            level_name,
+            file_name,
             today,
             uuid.uuid4()
         )
-        info = {"file": out_file, "level": level_id, "attributes": attributes}
+        info = {
+            "file": out_file,
+            "adm_id": adm_id,
+            "attributes": attributes
+        }
         job = Jobs.objects.create(
             type=JobTypes.download_administration,
             user_id=options.get("user")[0],
