66698c1c256308aac7fefbacc9a145cac680c64e
ifirmawan <firmawaneiwan@gmail.com>
2024-02-14 18:33:12 +0700

    [#1153] Close all cascade database connection when exit Form

--- app/src/pages/FormPage.js
+++ app/src/pages/FormPage.js
@@ -9,6 +9,8 @@ import {
 } from 'react-native';
 import { Button, Dialog, Text } from '@rneui/themed';
 import Icon from 'react-native-vector-icons/Ionicons';
+import * as SQLite from 'expo-sqlite';
+
 import { FormContainer } from '../form';
 import { SaveDialogMenu, SaveDropdownMenu } from '../form/support';
 import { BaseLayout } from '../components';
@@ -40,7 +42,17 @@ const FormPage = ({ navigation, route }) => {
   const [currentDataPoint, setCurrentDataPoint] = useState({});
   const [loading, setLoading] = useState(false);
 
+  const closeAllCascades = () => {
+    const { cascades: cascadesFiles } = formJSON;
+    cascadesFiles.forEach((csFile) => {
+      const [dbFile] = csFile?.split('/')?.slice(-1);
+      const connDB = SQLite.openDatabase(dbFile);
+      connDB.closeAsync();
+    });
+  };
+
   const refreshForm = () => {
+    closeAllCascades();
     FormState.update((s) => {
       s.currentValues = {};
       s.visitedQuestionGroup = [];
