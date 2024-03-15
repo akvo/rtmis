a79b7476f25fa83a25b110031472905305c430eb
ifirmawan <firmawaneiwan@gmail.com>
2024-03-15 16:52:46 +0700

    [#1146] Passing build.test.js by adding simple test case

--- app/src/build.test.js
+++ app/src/build.test.js
@@ -5,4 +5,8 @@ const defaultBuildParams = {
   serverURL: 'https://rtmis.akvotest.org/api/v1/device',
 };
 
+it('should at least one test', () => {
+  expect(defaultBuildParams.serverURL).toEqual('https://rtmis.akvotest.org/api/v1/device');
+});
+
 export default defaultBuildParams;
