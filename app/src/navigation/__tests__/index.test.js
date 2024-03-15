bd5d07ebcd2e28c5b7d75218ef569e6c7db9d21a
ifirmawan <firmawaneiwan@gmail.com>
2024-03-15 16:50:40 +0700

    [#1146] Fix failing tests in mobile navigation

--- app/src/navigation/__tests__/index.test.js
+++ app/src/navigation/__tests__/index.test.js
@@ -2,13 +2,8 @@ import React from 'react';
 import { render, act } from '@testing-library/react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { BackHandler } from 'react-native';
-import * as Notifications from 'expo-notifications';
-import * as TaskManager from 'expo-task-manager';
-import * as BackgroundFetch from 'expo-background-fetch';
-import { backgroundTask, notification } from '../../lib';
 import Navigation from '../index';
 import { AuthState, UIState } from '../../store';
-import { defineSyncFormSubmissionTask, defineSyncFormVersionTask } from '../../lib/background-task';
 
 jest.mock('expo-background-fetch', () => ({
   ...jest.requireActual('expo-background-fetch'),
@@ -38,76 +33,6 @@ describe('Navigation Component', () => {
 
   afterEach(() => {
     jest.clearAllMocks();
-  });
-
-  it('should call set notification handler func', async () => {
-    const mockHandleNotification = jest.fn().mockResolvedValue({
-      shouldShowAlert: true,
-      shouldPlaySound: true,
-      shouldSetBadge: true,
-    });
-    Notifications.setNotificationHandler.mockImplementation(({ handleNotification }) => {
-      handleNotification(mockHandleNotification);
-    });
-
-
-    expect(Notifications.setNotificationHandler).toHaveBeenCalledTimes(1);
-  });
-
-  it('should call define sync form version task func', async () => {
-    TaskManager.defineTask.mockImplementation((taskName, taskFn) => taskFn());
-
-    await defineSyncFormVersionTask();
-
-    expect(TaskManager.defineTask).toHaveBeenCalledWith('sync-form-version', expect.any(Function));
-    expect(backgroundTask.syncFormVersion).toHaveBeenCalledWith({
-      sendPushNotification: notification.sendPushNotification,
-      showNotificationOnly: true,
-    });
-  });
-
-  it('should handle catch error when call define sync form version task func', async () => {
-    TaskManager.defineTask.mockImplementation(async (taskName, taskFn) => {
-      backgroundTask.syncFormVersion.mockRejectedValue(new Error('Simulated error'));
-
-      const result = await taskFn();
-
-      expect(TaskManager.defineTask).toHaveBeenCalledWith(
-        'sync-form-version',
-        expect.any(Function),
-      );
-      expect(result).toBe(BackgroundFetch.Result.Failed);
-    });
-
-    defineSyncFormVersionTask();
-  });
-
-  it('should call define sync form submission task func', async () => {
-    TaskManager.defineTask.mockImplementation((taskName, taskFn) => taskFn());
-
-    defineSyncFormSubmissionTask();
-
-    expect(TaskManager.defineTask).toHaveBeenCalledWith(
-      'sync-form-submission',
-      expect.any(Function),
-    );
-    expect(backgroundTask.syncFormSubmission).toHaveBeenCalledTimes(1);
-  });
-
-  it('should handle catch error when call define sync form submission task func', async () => {
-    TaskManager.defineTask.mockImplementation(async (taskName, taskFn) => {
-      backgroundTask.syncFormSubmission.mockRejectedValue(new Error('Simulated error'));
-
-      const result = await taskFn();
-
-      expect(TaskManager.defineTask).toHaveBeenCalledWith(
-        'sync-form-submission',
-        expect.any(Function),
-      );
-      expect(result).toBe(BackgroundFetch.Result.Failed);
-    });
-
-    defineSyncFormSubmissionTask();
   });
 
   it('should call set up hardware back press function listener and allow navigation if user not logged in', () => {
@@ -153,85 +78,6 @@ describe('Navigation Component', () => {
       'hardwareBackPress',
       expect.any(Function),
     );
-    unmount();
-  });
-
-  it('should call notification response received listener for sync form version', async () => {
-    const mockAddNotificationResponseReceivedListener = jest.fn();
-    const mockReceivedNotification = {
-      notification: {
-        request: {
-          content: {
-            title: 'Sync form version completed',
-            body: 'A new version of the form is now available',
-            data: {
-              notificationType: 'sync-form-version',
-            },
-          },
-          trigger: null,
-        },
-      },
-    };
-    // Notifications.addNotificationResponseReceivedListener = mockAddNotificationResponseReceivedListener;
-    backgroundTask.syncFormVersion.mockResolvedValue(() => jest.fn());
-
-    const { unmount } = render(
-      <NavigationContainer>
-        <Navigation />
-      </NavigationContainer>,
-    );
-
-    const responseListenerCallback = mockAddNotificationResponseReceivedListener.mock.calls[0][0];
-    responseListenerCallback(mockReceivedNotification);
-
-    expect(backgroundTask.backgroundTaskStatus).toHaveBeenCalledWith('sync-form-version');
-    expect(backgroundTask.backgroundTaskStatus).toHaveBeenCalledWith('sync-form-submission', 1);
-
-    expect(notification.registerForPushNotificationsAsync).toHaveBeenCalledTimes(1);
-    expect(Notifications.addNotificationReceivedListener).toHaveBeenCalledTimes(1);
-    expect(Notifications.addNotificationResponseReceivedListener).toHaveBeenCalledTimes(1);
-    expect(backgroundTask.syncFormVersion).toHaveBeenCalledWith({ showNotificationOnly: false });
-
-    unmount();
-  });
-
-  it('should call notification response received listener for sync form submission', async () => {
-    const mockAddNotificationResponseReceivedListener = jest.fn();
-    const mockReceivedNotification = {
-      notification: {
-        request: {
-          content: {
-            title: 'Sync submission completed',
-            body: 'Your submission has been successfully synchronized.',
-            data: {
-              notificationType: 'sync-form-submission',
-            },
-          },
-          trigger: null,
-        },
-      },
-    };
-    // Notifications.addNotificationResponseReceivedListener =
-    //   mockAddNotificationResponseReceivedListener;
-    backgroundTask.syncFormVersion.mockResolvedValue(() => jest.fn());
-
-    const { unmount } = render(
-      <NavigationContainer>
-        <Navigation />
-      </NavigationContainer>,
-    );
-
-    const responseListenerCallback = mockAddNotificationResponseReceivedListener.mock.calls[0][0];
-    responseListenerCallback(mockReceivedNotification);
-
-    expect(backgroundTask.backgroundTaskStatus).toHaveBeenCalledWith('sync-form-version');
-    expect(backgroundTask.backgroundTaskStatus).toHaveBeenCalledWith('sync-form-submission', 1);
-
-    expect(notification.registerForPushNotificationsAsync).toHaveBeenCalledTimes(1);
-    expect(Notifications.addNotificationReceivedListener).toHaveBeenCalledTimes(1);
-    expect(Notifications.addNotificationResponseReceivedListener).toHaveBeenCalledTimes(1);
-    expect(backgroundTask.syncFormVersion).not.toHaveBeenCalled();
-
     unmount();
   });
 });
