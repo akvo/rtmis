1d4a5196569ad4754ab5517925f796f91bf43dfa
dedenbangkit <mail@dedenbangkit.com>
2024-03-18 23:03:25 +0700

    [#1255] Rename the build test and update the mobile pipeline

--- app/src/build.testing.js
+++ app/src/build.testing.js
@@ -5,8 +5,4 @@ const defaultBuildParams = {
   serverURL: 'https://rtmis.akvotest.org/api/v1/device',
 };
 
-it('should at least one test', () => {
-  expect(defaultBuildParams.serverURL).toEqual('https://rtmis.akvotest.org/api/v1/device');
-});
-
 export default defaultBuildParams;
