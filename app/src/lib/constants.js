725eb2c81324121f41f6bd5ce1e6edb59abf22f4
ifirmawan <firmawaneiwan@gmail.com>
2024-03-28 10:57:31 +0700

    [#1296] Create new file constants.js
    This file contains constant values used throughout the mobile app

---
+++ app/src/lib/constants.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/prefer-default-export */
+
+export const SYNC_FORM_VERSION_TASK_NAME = 'sync-form-version';
+
+export const SYNC_FORM_SUBMISSION_TASK_NAME = 'sync-form-submission';
+
+export const SYNC_STATUS = {
+  on_progress: 1,
+  re_sync: 2,
+  success: 3,
+};
+
+export const SUBMISSION_TYPES = {
+  registration: 1,
+  monitoring: 2,
+  verification: 3,
+  certification: 4,
+};
