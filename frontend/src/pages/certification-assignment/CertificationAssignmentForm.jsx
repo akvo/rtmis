1e5f5bc8919f07730051ad781b1b870d11664b1a
ifirmawan <firmawaneiwan@gmail.com>
2024-04-02 16:48:31 +0700

    [#1301] Fix invalid certifying cascade & add some styles

--- frontend/src/pages/certification-assignment/CertificationAssignmentForm.jsx
+++ frontend/src/pages/certification-assignment/CertificationAssignmentForm.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useMemo, useCallback, useEffect } from "react";
-import { Row, Col, Space, Form, Button, Select, Modal, Input } from "antd";
-import { MinusCircleOutlined } from "@ant-design/icons";
+import { Row, Col, Space, Form, Button, Select, Modal, Input, Tag } from "antd";
 import { useNavigate, useParams } from "react-router-dom";
 import {
   AdministrationDropdown,
@@ -9,6 +8,7 @@ import {
 } from "../../components";
 import { api, store, uiText } from "../../lib";
 import { useNotification } from "../../util/hooks";
+import "./style.scss";
 
 const CertificationAssignmentForm = () => {
   const [form] = Form.useForm();
@@ -22,6 +22,8 @@ const CertificationAssignmentForm = () => {
   const [loading, setLoading] = useState(true);
   const [counties, setCounties] = useState([]);
   const [subCounties, setSubCounties] = useState(null);
+  const [county, setCounty] = useState(null);
+  const [assignee, setAssignee] = useState(null);
 
   const profile = store.useState((s) => s.user);
   const selectedAdm = store.useState((s) => s.administration);
@@ -33,7 +35,7 @@ const CertificationAssignmentForm = () => {
     return uiText[activeLang];
   }, [activeLang]);
 
-  const onFinish = async ({ assignee, administrations }) => {
+  const onFinish = async ({ administrations }) => {
     setSubmitting(true);
     const administrationIDs = administrations?.map((a) => a?.id);
     try {
@@ -85,32 +87,72 @@ const CertificationAssignmentForm = () => {
     }
   };
 
-  const onSelectCounty = useCallback(async (countyID) => {
-    const { data: apiData } = await api.get(`/administrations/${countyID}`);
-    const subOptions = apiData?.children?.map((d) => ({
-      label: d?.name,
-      value: d?.id,
-    }));
-    setSubCounties(subOptions);
-  }, []);
+  const onSelectCounty = useCallback(
+    async (countyID) => {
+      setAssignee(null);
+      setSubCounties([]);
+      setCounty(countyID);
+      const { data: apiData } = await api.get(`/administrations/${countyID}`);
+      if (!IS_COUNTY_ADMIN) {
+        store.update((s) => {
+          s.administration = [s.administration[0], apiData];
+        });
+      }
+      const subOptions = apiData?.children?.map((d) => ({
+        label: d?.name,
+        value: d?.id,
+      }));
+      setSubCounties(subOptions);
+    },
+    [IS_COUNTY_ADMIN]
+  );
+
+  const onSelectSubCounty = (value) => {
+    setAssignee(value);
+    form.setFieldsValue({
+      assignee: value,
+      administrations: [],
+    });
+    store.update((s) => {
+      s.administration = selectedAdm.slice(0, 2);
+    });
+  };
 
   const onSelectVillage = (values) => {
     const lastLevel = levels?.slice(-1)?.[0]?.level;
     const villages = form.getFieldValue("administrations") || [];
-    if (selectedAdm?.length >= lastLevel) {
-      const sliceKey = selectedAdm.length === lastLevel ? -1 : -2;
-      const ward = selectedAdm?.slice(sliceKey)?.[0] || {};
+    const selectedLevel = selectedAdm?.slice(-1)?.[0]?.level + 1;
+    if (selectedLevel >= lastLevel) {
+      const ward = selectedAdm.find((s) => s?.level === lastLevel - 1);
       const village = ward?.children?.find(
         (c) =>
           values?.includes(c.id) &&
           !villages.filter((v) => v?.id === c?.id).length
       );
       if (village) {
-        form.setFieldValue("administrations", [...villages, village]);
+        form.setFieldValue("administrations", [
+          ...villages,
+          {
+            ...village,
+            full_name: village?.full_name?.replace(/\|/g, " - "),
+          },
+        ]);
         store.update((s) => {
-          s.administration = selectedAdm.slice(0, 1);
+          s.administration = selectedAdm.slice(0, 2);
         });
       }
+    }
+    /**
+     * Reset if change county
+     */
+    const findCounty = selectedAdm?.[0]?.children?.find((c) =>
+      values?.includes(c?.id)
+    );
+    if (findCounty && findCounty.id !== county) {
+      setCounty(findCounty.id);
+      setAssignee(null);
+      setSubCounties([]);
+      form.setFieldValue("administrations", []);
     }
   };
 
@@ -139,8 +181,9 @@ const CertificationAssignmentForm = () => {
       }
       if (id && loading && editAssignment?.id) {
         await onSelectCounty(editAssignment?.county_id);
+        setCounty(editAssignment?.county_id);
+        setAssignee(editAssignment?.assignee?.id);
         form.setFieldsValue({
-          county: editAssignment?.county_id,
           assignee: editAssignment?.assignee?.id,
           administrations: editAssignment?.administrations,
         });
@@ -196,7 +239,7 @@ const CertificationAssignmentForm = () => {
   }, [fetchSubCounties]);
 
   return (
-    <div id="mobile-assignments">
+    <div id="certification-assignments">
       <div className="description-container">
         <Row justify="space-between" align="bottom">
           <Col>
@@ -218,30 +261,6 @@ const CertificationAssignmentForm = () => {
             wrapperCol={{ span: 18 }}
             onFinish={onFinish}
           >
-            {profile?.role?.id === 1 && (
-              <div className="form-row">
-                <Form.Item
-                  name="county"
-                  label={text.admCounty}
-                  rules={[{ required: true, message: text.admCountyRequired }]}
-                >
-                  <Select
-                    placeholder={text.admCounty}
-                    getPopupContainer={(trigger) => trigger.parentNode}
-                    dropdownMatchSelectWidth={false}
-                    options={counties}
-                    className="custom-select"
-                    filterOption={(val, option) =>
-                      option.label.toLowerCase().includes(val.toLowerCase())
-                    }
-                    disabled={loading || submitting}
-                    onChange={onSelectCounty}
-                    showSearch
-                    allowClear
-                  />
-                </Form.Item>
-              </div>
-            )}
             <div className="form-row">
               <Form.Item
                 name="assignee"
@@ -250,38 +269,54 @@ const CertificationAssignmentForm = () => {
                   { required: true, message: text.certifyAssigneeRequired },
                 ]}
               >
-                <Select
-                  placeholder={text.certifyAssignee}
-                  getPopupContainer={(trigger) => trigger.parentNode}
-                  dropdownMatchSelectWidth={false}
-                  options={subCounties}
-                  className="custom-select"
-                  filterOption={(val, option) =>
-                    option.label.toLowerCase().includes(val.toLowerCase())
-                  }
-                  disabled={loading || submitting}
-                  showSearch
-                  allowClear
-                />
+                <Space align="baseline" direction="vertical" size="small">
+                  {!IS_COUNTY_ADMIN && (
+                    <Select
+                      placeholder={text.selectCounty}
+                      getPopupContainer={(trigger) => trigger.parentNode}
+                      dropdownMatchSelectWidth={false}
+                      options={counties}
+                      className="custom-select"
+                      filterOption={(val, option) =>
+                        option.label.toLowerCase().includes(val.toLowerCase())
+                      }
+                      disabled={loading || submitting}
+                      onChange={onSelectCounty}
+                      value={county}
+                      showSearch
+                      allowClear
+                    />
+                  )}
+                  <Select
+                    placeholder={text.selectSubCounty}
+                    getPopupContainer={(trigger) => trigger.parentNode}
+                    dropdownMatchSelectWidth={false}
+                    options={subCounties}
+                    className="custom-select"
+                    filterOption={(val, option) =>
+                      option.label.toLowerCase().includes(val.toLowerCase())
+                    }
+                    disabled={loading || submitting}
+                    onChange={onSelectSubCounty}
+                    value={assignee}
+                    showSearch
+                    allowClear
+                  />
+                </Space>
               </Form.Item>
             </div>
             <div className="form-row">
-              <Form.Item name="village" label={text.selectVillage}>
+              <Form.Item name="village" label={text.admToCertify}>
                 <AdministrationDropdown
                   size="large"
                   width="100%"
-                  direction="vertical"
-                  certify={
-                    IS_COUNTY_ADMIN
-                      ? profile.administration.id
-                      : form.getFieldValue("county")
-                  }
+                  certify={assignee}
                   onChange={onSelectVillage}
                 />
               </Form.Item>
             </div>
-            <div className="form-row">
-              <Form.Item label={text.admToCertify}>
+            <Row>
+              <Col span={18} offset={6} className="administrations-list">
                 <Form.List
                   name="administrations"
                   rules={[
@@ -292,28 +327,28 @@ const CertificationAssignmentForm = () => {
                   ]}
                 >
                   {(fields, { remove }, { errors }) => (
-                    <Space align="baseline" direction="vertical">
+                    <Space align="baseline" direction="vertical" size="small">
                       {fields.map(({ key, name, ...restField }) => (
-                        <Space align="baseline" key={key}>
-                          <Form.Item
-                            name={[name, "full_name"]}
-                            {...restField}
-                            style={{ width: "100%" }}
-                          >
-                            <Input style={{ minWidth: 600 }} readOnly />
+                        <div key={key}>
+                          <Form.Item name={[name, "full_name"]} {...restField}>
+                            <Input type="hidden" />
                           </Form.Item>
-                          <Button
-                            icon={<MinusCircleOutlined />}
-                            onClick={() => remove(name)}
-                          />
-                        </Space>
+                          <Tag onClose={() => remove(name)} closable>
+                            {form.getFieldValue([
+                              "administrations",
+                              name,
+                              "full_name",
+                            ])}
+                          </Tag>
+                        </div>
                       ))}
                       <Form.ErrorList errors={errors} />
                     </Space>
                   )}
                 </Form.List>
-              </Form.Item>
-            </div>
+              </Col>
+            </Row>
+
             <Row justify="center" align="middle">
               <Col span={18} offset={6}>
                 <Space>
