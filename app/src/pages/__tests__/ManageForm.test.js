996efa7ec96640fefc0e20797ac00bcecfd8c7a6
ifirmawan <firmawaneiwan@gmail.com>
2024-04-09 12:16:19 +0700

    [#1320] Remove unused waitFor in ManageForm test

--- app/src/pages/__tests__/ManageForm.test.js
+++ app/src/pages/__tests__/ManageForm.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer, { act } from 'react-test-renderer';
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 import { useNavigation } from '@react-navigation/native';
 import ManageFormPage from '../ManageForm';
 import { FormState, UserState } from '../../store';
