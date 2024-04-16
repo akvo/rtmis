694b2c4b3a303651e191f0263aafda7cc21c6503
ifirmawan <firmawaneiwan@gmail.com>
2024-04-16 18:56:25 +0700

    [#1344] Add submissionType prop in Card

--- app/src/components/Card.js
+++ app/src/components/Card.js
@@ -1,25 +1,67 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
+import { View } from 'react-native';
 import { Card as RneCard, Text } from '@rneui/themed';
 import PropTypes from 'prop-types';
+import { helpers } from '../lib';
+import { SUBMISSION_TYPES } from '../lib/constants';
 
-const Card = ({ title, subTitles }) => (
-  <RneCard>
-    {title && <RneCard.Title style={{ textAlign: 'left' }}>{title}</RneCard.Title>}
-    {subTitles?.map((s, sx) => (
-      <Text key={sx}>{s}</Text>
-    ))}
-  </RneCard>
-);
+const Card = ({ title, subTitles, submissionType }) => {
+  const subNames = helpers.flipObject(SUBMISSION_TYPES);
+  const subTypeName = Object.values(SUBMISSION_TYPES).includes(submissionType)
+    ? subNames[submissionType]
+    : subNames[SUBMISSION_TYPES.registration];
+  const colors = {
+    [SUBMISSION_TYPES.registration]: '#16a34a',
+    [SUBMISSION_TYPES.monitoring]: '#ca8a04',
+    [SUBMISSION_TYPES.verification]: '#2563eb',
+    [SUBMISSION_TYPES.certification]: '#7c3aed',
+  };
+  return (
+    <RneCard>
+      {title && <RneCard.Title style={{ textAlign: 'left' }}>{title}</RneCard.Title>}
+      {subTitles?.map((s, sx) => (
+        <Text key={sx}>{s}</Text>
+      ))}
+      {submissionType && (
+        <View
+          style={{
+            alignSelf: 'flex-start',
+            paddingVertical: 4,
+            paddingHorizontal: 12,
+            backgroundColor: colors?.[submissionType] || colors[SUBMISSION_TYPES.registration],
+            borderRadius: 16,
+            marginTop: 8,
+          }}
+          testID="submission-type-tag"
+        >
+          <Text
+            style={{
+              fontSize: 12,
+              width: '50%',
+              color: '#ffffff',
+              letterSpacing: 1.2,
+              fontWeight: '700',
+            }}
+          >
+            {helpers.capitalizeFirstLetter(subTypeName)}
+          </Text>
+        </View>
+      )}
+    </RneCard>
+  );
+};
 
 export default Card;
 
 Card.propTypes = {
   title: PropTypes.string,
   subTitles: PropTypes.arrayOf(PropTypes.string),
+  submissionType: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 };
 
 Card.defaultProps = {
   title: null,
   subTitles: [],
+  submissionType: null,
 };
