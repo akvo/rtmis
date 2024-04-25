3ce385599766f081cf7205c9d61efaf38fba63bd
dedenbangkit <mail@dedenbangkit.com>
2024-04-25 08:42:19 +0700

    Modify client_max_body_size

--- backend/run-prod.sh
+++ backend/run-prod.sh
@@ -8,21 +8,19 @@ python manage.py generate_views
 python manage.py generate_sqlite >/dev/null &
 python manage.py generate_config >/dev/null &
 
-python manage.py runserver 0.0.0.0:8000
+function log {
+    echo "$(date +"%T") - START INFO - $*"
+}
 
-# function log {
-#     echo "$(date +"%T") - START INFO - $*"
-# }
-#
-# _term() {
-#     echo "Caught SIGTERM signal!"
-#     kill -TERM "$child" 2>/dev/null
-# }
-#
-# trap _term SIGTERM
-#
-# log Starting gunicorn in background
-# gunicorn rtmis.wsgi --workers 6 --timeout 300 --bind 0.0.0.0:8000 >>./logs/gunicorn.log 2>&1 &
-#
-# child=$!
-# wait "$child"
+_term() {
+    echo "Caught SIGTERM signal!"
+    kill -TERM "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+
+log Starting gunicorn in background
+gunicorn rtmis.wsgi --workers 6 --timeout 300 --bind 0.0.0.0:8000 &
+
+child=$!
+wait "$child"
