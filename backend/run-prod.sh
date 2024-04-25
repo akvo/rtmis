50fb3d45dde5dba016bb80857fa31204a4fe6e6b
dedenbangkit <mail@dedenbangkit.com>
2024-04-25 07:29:28 +0700

    Store gunicorn log

--- backend/run-prod.sh
+++ backend/run-prod.sh
@@ -5,22 +5,22 @@ set -eu
 
 python manage.py migrate
 python manage.py generate_views
-python manage.py generate_sqlite > /dev/null &
-python manage.py generate_config > /dev/null &
+python manage.py generate_sqlite >/dev/null &
+python manage.py generate_config >/dev/null &
 
 function log {
-   echo "$(date +"%T") - START INFO - $*"
+    echo "$(date +"%T") - START INFO - $*"
 }
 
 _term() {
-  echo "Caught SIGTERM signal!"
-  kill -TERM "$child" 2>/dev/null
+    echo "Caught SIGTERM signal!"
+    kill -TERM "$child" 2>/dev/null
 }
 
 trap _term SIGTERM
 
 log Starting gunicorn in background
-gunicorn rtmis.wsgi --workers 6 --timeout 300 --bind 0.0.0.0:8000 &
+gunicorn rtmis.wsgi --workers 6 --timeout 300 --bind 0.0.0.0:8000 >>./logs/gunicorn.log 2>&1 &
 
 child=$!
 wait "$child"
