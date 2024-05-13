16e2bf7b9b69536c4236476dc64435352d63abe7
wayangalihpratama <wgprtm@gmail.com>
2024-05-13 16:27:19 +0800

    [#1459] Use version on package.json as base app version

--- app/replaceVersion.sh
+++ app/replaceVersion.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Extract appVersion from build.json
-appVersion=$(grep -o '"appVersion": *"[^"]*"' src/build.json | cut -d'"' -f4)
+appVersion=$(grep -o '"version": *"[^"]*"' package.json | cut -d'"' -f4)
+
+# Update versionCode in src/build.json
+sed -i.bak "s/\"appVersion\": *\"[^\"]*\"/\"appVersion\": \"$appVersion\"/" src/build.json
 
 # Update versionCode in app.json
 sed -i.bak "s/\"versionCode\": *\"[^\"]*\"/\"versionCode\": \"$appVersion\"/" app.json
 
 # Remove the backup file created by sed
+rm src/build.json.bak
 rm app.json.bak
