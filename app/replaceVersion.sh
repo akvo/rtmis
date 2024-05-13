8145110b27f9a642053b9cdbf044d94b4070a5c3
wayangalihpratama <wgprtm@gmail.com>
2024-05-13 17:50:43 +0800

    [#1459] Update expo version with package.json version & convert string version into number versionCode for android

--- app/replaceVersion.sh
+++ app/replaceVersion.sh
@@ -2,13 +2,16 @@
 
 # Extract appVersion from build.json
 appVersion=$(grep -o '"version": *"[^"]*"' package.json | cut -d'"' -f4)
+versionInteger=$(echo "$appVersion" | tr -d '.')
 
 # Update versionCode in src/build.json
 sed -i.bak "s/\"appVersion\": *\"[^\"]*\"/\"appVersion\": \"$appVersion\"/" src/build.json
-
-# Update versionCode in app.json
-sed -i.bak "s/\"versionCode\": *\"[^\"]*\"/\"versionCode\": \"$appVersion\"/" app.json
-
-# Remove the backup file created by sed
 rm src/build.json.bak
+
+# Update version and versionCode in app.json
+sed -i.bak "s/\"version\": *\"[^\"]*\"/\"version\": \"$appVersion\"/" app.json
 rm app.json.bak
+sed -i.bak "s/\"versionCode\": *\"[^\"]*\"/\"versionCode\": $versionInteger/" app.json
+rm app.json.bak
+
+echo "Updated to version:" $appVersion
