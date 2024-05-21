7feed1c0d5269a1456d616c981e2bb4bd9e74594
Jonah Kisioh <jkisioh@gmail.com>
2024-05-21 09:59:30 +0300

    Fix - removed missing questions in outcome result formulas

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,14 +4,14 @@ module.exports = function (app) {
   app.use(
     ["/api/**", "/static-files/**"],
     createProxyMiddleware({
-      target: "http://localhost:8000",
+      target: "http://127.0.0.1:8000",
       changeOrigin: true,
     })
   );
   app.use(
     ["/config.js"],
     createProxyMiddleware({
-      target: "http://localhost:8000",
+      target: "http://127.0.0.1:8000",
       changeOrigin: true,
       secure: false,
       pathRewrite: {
@@ -22,7 +22,7 @@ module.exports = function (app) {
   app.use(
     ["/app"],
     createProxyMiddleware({
-      target: "http://localhost:3000",
+      target: "http://127.0.0.1:3000",
       changeOrigin: true,
       pathRewrite: {
         "^/app": "/apk/rtmis.apk",
@@ -32,7 +32,7 @@ module.exports = function (app) {
   app.use(
     ["/master-data"],
     createProxyMiddleware({
-      target: "http://localhost:3000",
+      target: "http://127.0.0.1:3000",
       changeOrigin: true,
       pathRewrite: {
         "^/master-data": "/master_data/kenya-administration.csv",
