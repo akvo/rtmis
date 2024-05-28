5e58c8c326f0b353f72698a9a0552e436e0535be
ifirmawan <firmawaneiwan@gmail.com>
2024-05-28 18:30:55 +0700

    [#1515] Additional sentry config
    reference: https://docs.expo.dev/guides/using-sentry/#additional-configuration-options

--- app/jest.config.js
+++ app/jest.config.js
@@ -176,7 +176,7 @@ module.exports = {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['node_modules/(?!react-native|react-navigation)/'],
+  transformIgnorePatterns: ['node_modules/(?!react-native|react-navigation|@sentry|sentry-expo)/'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
